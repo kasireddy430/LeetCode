ea28b1b84b9e5f2f1e78f2331d5d3b82e65c5b66
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-01-18 15:02:03 -0800

    Time: 2 ms (99.87%), Space: 49.9 MB (33.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -11,22 +11,23 @@ class Solution {
         for(int r = 0; r < ROWS; r++){
             for(int c = 0; c < COLS; c++){
                 //Check if the CEll is an island or not
-                if(grid[r][c] == "1"){
-                    islandsCount++; //Count the valid islands
+                if(grid[r][c] == '1'){
+                    islandsCount++; //Increment the island count
                     visitIslands(grid, r, c);  //Mark the islands visited
                 }
             }
         }
+        return islandsCount;
     }
 
     private void visitIslands(char[][] grid, int r, int c){
         //Check if the cell is out of bounds or is it water
-        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == "0"){
+        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0'){
             return;
         }
 
         //Mark the island visited by marking it as water
-        grid[r][c] = "0";
+        grid[r][c] = '0';
 
         //Recursively visit all the neightbouring Cells 
         visitIslands(grid, r+1, c);
@@ -35,3 +36,6 @@ class Solution {
         visitIslands(grid, r, c-1);
     }
 }
+
+//Time Complexity: O(m * n)
+//Space Complexity: O(m * n). Contributed by the recursion stack
