4158166a9a40ab9215894613ac3a9791b2e91e11
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-01-17 16:03:56 -0800

    Time: 2 ms (98.93%), Space: 45.1 MB (27.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,24 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //Create a hashmap to store the integers from input array and their indices
         Map<Integer, Integer> values = new HashMap<>();
 
         for(int i = 0; i < nums.length; i++){
+            //Calaculate the target sum
             int targetSum = target - nums[i];
 
+            //Check if the target sum is part of the hash map
             if(values.containsKey(targetSum)){
+                //If so we found the valid pair 
                 return new int[]{values.get(targetSum), i};
             }
+            //If not add it to the map
             values.put(nums[i], i);
         }
+        //return empty array if no valid pair is found
         return new int[]{};
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
