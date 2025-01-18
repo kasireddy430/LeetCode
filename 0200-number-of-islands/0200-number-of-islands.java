5c588290a9e7eb40adb6bdf8959fa78a5aea1d9a
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-01-18 14:59:27 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        //Calculate row and column lengths of the 2-D matrix
+        int ROWS = grid.length;
+        int COLS = grid[0].length;
+
+        //Initialize a variable to count number of valid islands
+        int islandsCount = 0;
+
+        //Iterate over each cell
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                //Check if the CEll is an island or not
+                if(grid[r][c] == "1"){
+                    islandsCount++; //Count the valid islands
+                    visitIslands(grid, r, c);  //Mark the islands visited
+                }
+            }
+        }
+    }
+
+    private void visitIslands(char[][] grid, int r, int c){
+        //Check if the cell is out of bounds or is it water
+        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == "0"){
+            return;
+        }
+
+        //Mark the island visited by marking it as water
+        grid[r][c] = "0";
+
+        //Recursively visit all the neightbouring Cells 
+        visitIslands(grid, r+1, c);
+        visitIslands(grid, r-1, c);
+        visitIslands(grid, r, c+1);
+        visitIslands(grid, r, c-1);
+    }
+}
