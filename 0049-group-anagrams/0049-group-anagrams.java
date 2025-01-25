3e9993d7b3fe1cd552aac24094bbce06a40f68c1
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-01-25 15:10:53 -0800

    Time: 19 ms (20.11%), Space: 49.2 MB (10.15%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> grpAnagrams = new HashMap<>();
+        //Create a hashmap to store the list of anagram strings for a given key
+        Map<String, List<String>> anagram = new HashMap<>();
 
-        for(String s : strs){
+        //Iterate over input array
+        for(String str : strs){
+            //Initialize a count array for count the characters frequencies
             int[] count = new int[26];
 
-            for(char c : s.toCharArray()){
+            for(char c : str.toCharArray()){
                 count[c - 'a']++;
             }
 
+            //Convert character count array to a string and use it as a key
             String key = Arrays.toString(count);
-            grpAnagrams.computeIfAbsent(key, k -> new ArrayList<>());
-            grpAnagrams.get(key).add(s);
+            //Check if a given key does exists or not. If exists add the string to list of strings.
+            //If not, compute an arrayList and then add the string for the given key
+            anagram.putIfAbsent(key, new ArrayList<>());
+            anagram.get(key).add(str);
         }
-        return new ArrayList<>(grpAnagrams.values());
+        //return the list of anagram's grouped 
+        return new ArrayList<>(anagram.values());
     }
 }
+
+//Time Complexity: O(m * n)
+//Space Complexity: O(m)
+// Where m is the number of strings and n is the length of the longest string.
