224279b0d12b364a7a9785bbc4136446d7c6a078
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-01-29 20:39:10 -0800

    Time: 6 ms (8.59%), Space: 44.3 MB (67.91%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -15,18 +15,21 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        String rootSerialized = serialize(root);
-        String subRootSerialized = serialize(subRoot);
-
-        return rootSerialized.contains(subRootSerialized);
-    }
-
-    private String serialize(TreeNode root){
         if(root == null){
-            return "#";
+            return subRoot == null;
         }
 
-        return "," + root.val + "," + serialize(root.left) + "," + serialize(root.right);
+        return isIdentical(root, subRoot) || isSubtree(root.left, subRoot) 
+                || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isIdentical(TreeNode root, TreeNode subRoot){
+        if(root == null || subRoot == null){
+            return root == subRoot;
+        }
+
+        return (root.val == subRoot.val) && isIdentical(root.left, subRoot.left) && 
+            isIdentical(root.right, subRoot.right);
     }
 }
 
