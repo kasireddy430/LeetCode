dd43b4b9472a8349d9233e0fdf5a713394384c15
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-01-31 19:03:59 -0800

    Time: 5 ms (100%), Space: 45.1 MB (62.92%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,17 +10,25 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //Initialize a pointer at root
         TreeNode cur = root;
-        
+
+        //Iterate until cur reaches null
         while(cur != null){
+            //Check if p and q nodes values are less than current node's value
             if(p.val < cur.val && q.val < cur.val){
+                //If so, LCA could be found in left subtree
                 cur = cur.left;
-            } else if(p.val > cur.val && q.val > cur.val){
+            }//Check if p and q nodes values are greater than current node's value
+             else if(p.val > cur.val && q.val > cur.val){
+                //If so, LCA could be found in right subtree
                 cur = cur.right;
             } else{
+                //LCA found
                 return cur;
             }
         }
+        //return null, if LCA isn't found
         return null;
     }
 }
