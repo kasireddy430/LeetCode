d23f20408c174c2f2f3f0ceba1ca635cb23de717
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-02-27 19:23:53 -0800

    Time: 0 ms (100%), Space: 41.1 MB (97.36%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        //Initialize leftmin and leftmax variables for tracking minimum and maximum left parenthesis
+        int leftMin = 0, leftMax = 0;
+
+        //Iterate over each character in input string
+        for(char c : s.toCharArray()){
+            if(c == '('){//Check if it's open parenthesis
+                leftMin++;
+                leftMax++;
+            } else if(c == ')'){//Check if it's closed parenthesis
+                leftMin--;
+                leftMax--;
+            } else{//If it's a star it can be either '(' or ')'
+                leftMin--;//Star as '('
+                leftMax++;//star as ')'
+            }
+
+        if(leftMax < 0){ //Indicates number of closed parenthesis is greater than open paranthesis
+            return false;
+        }
+
+        //Number of open paranthesis cannot be negative, reset the min variable to 0
+        if(leftMin < 0){
+            leftMin = 0; 
+        }
+
+        }
+        return leftMin == 0;
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
