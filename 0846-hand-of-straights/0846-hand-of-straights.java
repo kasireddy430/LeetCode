104d320b547450b42ed58929d542a237cbad0c27
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-02-27 17:36:53 -0800

    Time: 62 ms (29.84%), Space: 45.9 MB (28.68%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        //Base case: If hand.length is not multiple of groupSize, then we cannot form equal groups
+        if(hand.length % groupSize != 0) return false;
+
+        //Create TreeMap to count frequencies of each card and it will be in sorted order
+        TreeMap<Integer, Integer> count = new TreeMap<>();
+        for(int num : hand){
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        //Process groups starting from the smallest available number
+        while(!count.isEmpty()){
+            //get the smallest card from treemap
+            int small = count.firstKey();
+
+            //Try to form groups of groupSize
+            for(int i = small; i < small + groupSize; i++){
+                //Return false if there is number missing, which indicates equal groups cannot be formed
+                if(!count.containsKey(i)){
+                    return false;
+                }
+
+                //Decrement the count frequency, since the card is used to form group
+                count.put(i, count.get(i) - 1);
+                //Remove vard from TreeMap if count reaches 0
+                if(count.get(i)==0){
+                    count.remove(i);
+                }
+            }
+        }
+        //Return true if exits the loop, indicating equal groups
+        return true;
+    }
+}
+
+//Time Complexity: O(n log n)
+//Space Complexity: O(n)
