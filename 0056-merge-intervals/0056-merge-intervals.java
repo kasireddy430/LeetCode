3906ed3480d51cf25054ebc5fe9f013ebf1937b2
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-02-28 16:13:22 -0800

    Time: 8 ms (87.11%), Space: 46.4 MB (82.87%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,14 +6,19 @@ class Solution {
         //Create a list to store the result of merged sorted list
         LinkedList<int[]> res = new LinkedList<>(); //using linkedList makes easier to extract last element from list
 
+        //Iterate over each interval
         for(int[] interval: intervals){
+            //Check if the result list is empty or check if previous
+            //interval does not overlap with current interval
             if(res.isEmpty() || res.getLast()[1] < interval[0]){
+                //If so both interval's aren't overlapping, add current interval to result list
                 res.add(interval);
-            } else {
+            } else {//If interval's meet, update the ending interval value
                 res.getLast()[1] = Math.max(res.getLast()[1], interval[1]);
             }
         }
 
+        //Convert result list to 2D array and return it
         return res.toArray(new int[res.size()][]);
     }
 }
