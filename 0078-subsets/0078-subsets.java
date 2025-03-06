5ccaa447ce1b7d9f756ae75405d54d65ea293cc2
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-06 15:00:16 -0800

    Time: 1 ms (53.83%), Space: 42.6 MB (81.56%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();//result stores list of all subsets
+        List<Integer> subset = new ArrayList<>();//stores current subset
+        dfs(nums, 0, subset, result);//recursively process elements starting from index '0'
+        return result;
+    }
+
+    private void dfs(int[] nums, int i, List<Integer> subset, List<List<Integer>> result){
+        //Base Case: if all elements are processed, add it to result
+        if(i >= nums.length){
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+
+        //Inculde current element and recurr to next index
+        subset.add(nums[i]);
+        dfs(nums, i + 1, subset, result);
+        //Exculde current element and recurr to next index
+        subset.remove(subset.size() - 1);
+        dfs(nums, i + 1, subset, result);
+    }
+}
+
+//Time Complexity: O(n * 2^n)
+//Space Complexity: O(n)
