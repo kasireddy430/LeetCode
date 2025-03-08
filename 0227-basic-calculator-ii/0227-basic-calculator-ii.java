ff56f1b2ae44dd87375209504b50d95a1dd5e445
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-07 16:57:30 -0800

    Time: 16 ms (75.94%), Space: 45.2 MB (73.56%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int calculate(String s) {
+        //Edge Case: Return 0 if the input string is null or empty
+        if(s == null || s.isEmpty()){
+            return 0;
+        }
+
+        int currentNumber = 0; //stores current number being processed(for handling multi-digit)
+        int lastNumber = 0;//stores last number processed
+        int result = 0;//stores final computed result
+        char operator = '+';//stores last operator encountered(starting with +)
+
+        //Iterate over each character in input array
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            //Check if character is digit
+            if(Character.isDigit(c)){
+                currentNumber = (currentNumber * 10) + (c - '0');
+            } 
+            
+            //Check if character is operator or the end of the string
+            if(!Character.isDigit(c) && !Character.isWhitespace(c) || i == s.length() - 1){
+                //if previous operator is + or - then add previously processed number to result
+                //and update lastNumber based on the previous operator
+                if(operator == '+' || operator == '-'){
+                    result += lastNumber;
+                    lastNumber = (operator == '+') ? currentNumber : -currentNumber;
+                } else if(operator == '*'){
+                    //if previous operator is '*' then process last and 
+                    //current numbers by multiplication
+                    lastNumber *= currentNumber;
+                } else if(operator == '/'){
+                    //if previous operator is '*' then process last and 
+                    //current numbers by division
+                    lastNumber /= currentNumber;
+                }
+                operator = c;
+                currentNumber = 0;
+            }
+        }
+
+        //return final result by adding last computed number
+        return result + lastNumber;
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
