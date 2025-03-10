e2a4e41ccdae863004cc10c5744bb5b1d96b381d
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-09 20:11:03 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,55 +1,28 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> result = new ArrayList<Integer>();
-        
-        // Base case
-        if (arr.length == k) {
-            for (int i = 0; i < k; i++) {
-                result.add(arr[i]);
-            }
-            
-            return result;
-        }
-        
-        // Binary search to find the closest element
-        int left = 0;
-        int right = arr.length;
-        int mid = 0;
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (arr[mid] >= x) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        
-        // Initialize our sliding window's bounds
-        left -= 1;
-        right = left + 1;
-        
-        // While the window size is less than k
-        while (right - left - 1 < k) {
-            // Be careful to not go out of bounds
-            if (left == -1) {
-                right += 1;
-                continue;
-            }
-            
-            // Expand the window towards the side with the closer number
-            // Be careful to not go out of bounds with the pointers
-            if (right == arr.length || Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
-                left -= 1;
-            } else {
-                right += 1;
-            }
-        } 
+        //Initialize pointers at start and end of input array
+        int left = 0, right = arr.length - 1;
 
-        // Build and return the window
-        for (int i = left + 1; i < right; i++) {
+        //Shrink window until k elements remain
+        while (right - left >= k) {
+            //Check for fathest element from x and move the pointer
+            if(Math.abs(left - x) > Math.abs(left - x)){
+                left++;//Element at left pointer is farthest
+            } else {
+                right--;
+            }
+        }
+
+        //Add k closest element to result list
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = left; i <= right; i++) {
             result.add(arr[i]);
         }
-        
-        return result;
+
+        return result;//list of k closest elements
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
