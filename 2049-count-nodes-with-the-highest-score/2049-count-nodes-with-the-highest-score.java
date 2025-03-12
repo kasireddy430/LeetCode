4a713ff8edd88c52bb53c79e85170b725804f120
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-11 22:05:24 -0700

    Time: 13 ms (99.51%), Space: 60.2 MB (92.44%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
@@ -1,35 +1,46 @@
 class Solution {
     public int countHighestScoreNodes(int[] parents) {
         int n = parents.length;
+
         int[] inDegree = new int[n];
-        for (int i = 1; i < n; i++) {
+
+        for(int i = 1; i < n ; i++){
             inDegree[parents[i]]++;
         }
-        int[] treeSize = new int[n], remain = new int[n];
+
+        int[] treeLength = new int[n], remain = new int[n];
         long[] score = new long[n];
-        Arrays.fill(treeSize, 1);
+
+        Arrays.fill(treeLength, 1);
         Arrays.fill(remain, n - 1);
         Arrays.fill(score, 1);
         int[] q = new int[n];
+        
         int head = 0, tail = 0;
-        for (int i = 0; i < n; i++) {
-            if (inDegree[i] == 0) {
+        for(int i = 0; i < n; i++){
+            if(inDegree[i] == 0){
                 q[head++] = i;
                 score[i] = n - 1;
             }
         }
-        while (tail < head) {
+
+        while(head > tail){
             int node = q[tail++];
             int pa = parents[node];
-            if (pa == -1) continue;
-            remain[pa] -= treeSize[node];
-            treeSize[pa] += treeSize[node];
-            score[pa] *= treeSize[node];
-            if (--inDegree[pa] == 0) {
+
+            if(pa == -1) continue;
+
+            remain[pa] -= treeLength[node];
+            treeLength[pa] += treeLength[node];
+            score[pa] *= treeLength[node];
+
+            if(--inDegree[pa] == 0){
                 q[head++] = pa;
                 if (pa != 0) score[pa] *= remain[pa];
-            }
+            }  
         }
+
+
         int ans = 0;
         long max = 0;
         for (long s : score) {
@@ -42,6 +53,4 @@ class Solution {
         }
         return ans;
     }
-
-
 }
