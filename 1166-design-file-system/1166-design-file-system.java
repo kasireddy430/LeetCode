274b82dbfff323df01a3b0c4d6c3a672e94f6992
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-12 12:43:18 -0700

    Time: 92 ms (61.58%), Space: 55.5 MB (81.17%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.java
+++ 1166-design-file-system/1166-design-file-system.java
@@ -1,25 +1,20 @@
 class FileSystem {
-    Map<String,Integer> map;
+    private Map<String, Integer> map;
     public FileSystem() {
         map = new HashMap<>();
-        map.put("", -1);
     }
     
     public boolean createPath(String path, int value) {
-        if (map.containsKey(path)) {
-            return false; // Path already exists
+        if (map.get(path) != null) {
+            return false;
         }
-
-        int lastSlash = path.lastIndexOf("/");
-        String parent = path.substring(0, lastSlash);
-
-        if (!map.containsKey(parent)) {
-            return false; // Parent path must exist
+        String parent = path.substring(0, path.lastIndexOf('/'));
+        if (!parent.isEmpty() && map.get(parent) == null) {
+            return false;
         }
 
         map.put(path, value);
         return true;
-        
     }
     
     public int get(String path) {
