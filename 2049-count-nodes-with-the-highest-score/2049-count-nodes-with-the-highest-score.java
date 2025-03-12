ecb93787157a31430af98feb7a5936df7468a7a7
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-11 17:20:05 -0700

    Time: 13 ms (99.51%), Space: 60 MB (94.39%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.java
@@ -1,23 +1,47 @@
 class Solution {
-long dfs(List<List<Integer>> al, long[] s, int i) {
-    long prod = 1, sum = 1;
-    for (int j : al.get(i)) {
-        long cnt = dfs(al, s, j);
-        prod *= cnt;
-        sum += cnt;
+    public int countHighestScoreNodes(int[] parents) {
+        int n = parents.length;
+        int[] inDegree = new int[n];
+        for (int i = 1; i < n; i++) {
+            inDegree[parents[i]]++;
+        }
+        int[] treeSize = new int[n], remain = new int[n];
+        long[] score = new long[n];
+        Arrays.fill(treeSize, 1);
+        Arrays.fill(remain, n - 1);
+        Arrays.fill(score, 1);
+        int[] q = new int[n];
+        int head = 0, tail = 0;
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                q[head++] = i;
+                score[i] = n - 1;
+            }
+        }
+        while (tail < head) {
+            int node = q[tail++];
+            int pa = parents[node];
+            if (pa == -1) continue;
+            remain[pa] -= treeSize[node];
+            treeSize[pa] += treeSize[node];
+            score[pa] *= treeSize[node];
+            if (--inDegree[pa] == 0) {
+                q[head++] = pa;
+                if (pa != 0) score[pa] *= remain[pa];
+            }
+        }
+        int ans = 0;
+        long max = 0;
+        for (long s : score) {
+            if (s == max) {
+                ans++;
+            } else if (max < s) {
+                ans = 1;
+                max = s;
+            }
+        }
+        return ans;
     }
-    s[i] = prod * (Math.max(1, al.size() - sum));
-    return sum;
-}    
-public int countHighestScoreNodes(int[] p) {
-    List<List<Integer>> al = new ArrayList<>();
-    for (int i = 0; i < p.length; ++i)
-        al.add(new ArrayList<Integer>());         
-    long[] s = new long[p.length];
-    for (int i = 1; i < p.length; ++i)
-        al.get(p[i]).add(i);
-    dfs(al, s, 0);
-    long max_val = Arrays.stream(s).max().getAsLong();
-    return (int)Arrays.stream(s).filter(v -> v == max_val).count();
-}
+
+
 }
