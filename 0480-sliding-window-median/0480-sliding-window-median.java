c784dd6e7bddf5c837abb7b586402bc42142a48a
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-12 15:28:42 -0700

    Time: 41 ms (88.95%), Space: 58.5 MB (5.87%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,78 +1,74 @@
 class Solution {
-    PriorityQueue<Integer> min = new PriorityQueue<>(Collections.reverseOrder()); // Max-heap for the lower half
-    PriorityQueue<Integer> max = new PriorityQueue<>(); // Min-heap for the upper half
-    HashMap<Integer, Integer> map = new HashMap<>(); // Hash-table for lazy removal
 
+    private final PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+    private final PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private final HashMap<Integer, Integer> map = new HashMap<>();
+    private int balance;
 
     public double[] medianSlidingWindow(int[] nums, int k) {
-        double[] medians = new double[nums.length - k + 1];
-        int i = 0;
-        while(i < k){
-            min.add(nums[i++]);
+        int n = nums.length;
+        var res = new double[n - k + 1];
+
+        for (int i = 0; i < k; i++) {
+            addNum(nums[i]);
         }
-        for(int j = 0; j < k /2; j++){
-            max.add(min.poll());
+
+        res[0] = findMedian();
+
+        for (int i = k; i < n; i++) {
+            removeNum(nums[i - k]);
+            addNum(nums[i]);
+
+            res[i - k + 1] = findMedian();
         }
-        
-        int itr = 0;
-        while(true){
-            medians[itr++] = findMedian(k);
-            
-            if(i >= nums.length) break;
-            
-            
-            int out = nums[i - k];
-            int in = nums[i++];
-            int balance = 0;
-            
-            balance += (out <= min.peek()? -1: 1);
-            map.put(out, map.getOrDefault(out, 0) + 1);
-            
-            if(!min.isEmpty() && in <= min.peek()){
-                balance++;
-                min.add(in);
-            }else{
-                balance--;
-                max.add(in);
-            }
-            
-            //rebalance
-            
-            if(balance < 0){
-                min.add(max.poll());
-                balance++;
-            }
-            
-            if(balance > 0){
-                max.add(min.poll());
-                balance--;
-            }
-            
-            //remove numbers
-            while(!min.isEmpty() && map.containsKey(min.peek())){
-                map.put(min.peek(), map.get(min.peek()) - 1);
-                if(map.get(min.peek()) == 0){
-                    map.remove(min.peek());
-                }
-                min.poll();
-            }
-            
-            while(!max.isEmpty() && map.containsKey(max.peek())){
-                map.put(max.peek(), map.get(max.peek()) - 1);
-                if(map.get(max.peek()) == 0){
-                    map.remove(max.peek());
-                }
-                max.poll();
-            }
-            
-            
-        }
-        return medians;
+
+        return res;
     }
 
-    private double findMedian(int k) {
-        if ((k & 1) == 1) return min.peek();
-        
-        return ((double) max.peek() + min.peek()) / 2.0;
+    private void addNum(int num) {
+        if (!maxHeap.isEmpty() && maxHeap.peek() >= num) {
+            maxHeap.offer(num);
+            balance++;
+        } else {
+            minHeap.offer(num);
+            balance--;
+        }
+    }
+
+    private void removeNum(int num) {
+        map.merge(num, 1, Integer::sum);
+
+        if (maxHeap.peek() >= num) {
+            balance--;
+        } else {
+            balance++;
+        }
+    }
+
+    private double findMedian() {
+        balance();
+        cleanHeap(maxHeap);
+        cleanHeap(minHeap);
+
+        // balance();
+
+        return balance > 0 ? maxHeap.peek() : ((long) maxHeap.peek() + minHeap.peek()) / 2.0;
+    }
+
+    private void cleanHeap(PriorityQueue<Integer> heap) {
+        while (map.containsKey(heap.peek())) {
+            map.merge(heap.poll(), 1, (curr, diff) -> curr != 1 ? curr - diff : null);
+        }
+    }
+
+    private void balance() {
+        while (balance > 1) {
+            minHeap.offer(maxHeap.poll());
+            balance -= 2;
+        }
+        while (balance < 0) {
+            maxHeap.offer(minHeap.poll());
+            balance += 2;
+        }
     }
 }
