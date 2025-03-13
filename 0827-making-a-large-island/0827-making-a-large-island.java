2cafc157b5b873d54fc84e407053a2c5cc165e62
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-12 20:29:02 -0700

    Time: 56 ms (83.53%), Space: 74.1 MB (71.9%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,71 +1,58 @@
-
-class Solution {
-    private int n;
-    private int[][] grid;
-    private Map<Integer, Integer> islandSizes = new HashMap<>();
-    private static final int[][] DIRECTIONS = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+public class Solution {
+    private static final int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int largestIsland(int[][] grid) {
-        this.n = grid.length;
-        this.grid = grid;
-        int color = 2; // Start coloring islands from 2
-        int maxIsland = 0;
-        boolean hasZero = false;
+        int n = grid.length;
+        List<Integer> key = new ArrayList<>();
+        int id = 2;
 
-        // Step 1: Identify and color each island while storing their sizes
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    int size = dfs(i, j, color);
-                    islandSizes.put(color, size);
-                    maxIsland = Math.max(maxIsland, size);
-                    color++;
-                } else {
-                    hasZero = true;
+                    int size = dfs(grid, i, j, id);
+                    key.add(size);
+                    id++;
                 }
             }
         }
 
-        // If the grid has no zero, return the size of the entire grid
-        if (!hasZero) return maxIsland;
+        if (key.isEmpty()) return 1;
 
-        // Step 2: Try converting each 0 into a 1 and compute the largest possible island
+        int max = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
-                    Set<Integer> visitedIslands = new HashSet<>();
-                    int newSize = 1; // Start with 1 (since we are flipping a zero)
-                    
-                    // Check all 4 directions for adjacent islands
-                    for (int[] dir : DIRECTIONS) {
-                        int x = i + dir[0], y = j + dir[1];
-                        if (isValid(x, y) && grid[x][y] > 1 && visitedIslands.add(grid[x][y])) {
-                            newSize += islandSizes.get(grid[x][y]);
+                    Set<Integer> seen = new HashSet<>();
+                    int sum = 1;
+                    for (int[] dir : dirs) {
+                        int ni = i + dir[0];
+                        int nj = j + dir[1];
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < n && grid[ni][nj] >= 2) {
+                            int islandId = grid[ni][nj];
+                            if (!seen.contains(islandId)) {
+                                sum += key.get(islandId - 2);
+                                seen.add(islandId);
+                            }
                         }
                     }
-                    
-                    maxIsland = Math.max(maxIsland, newSize);
+                    max = Math.max(max, sum);
                 }
             }
         }
-        
-        return maxIsland;
+
+        return max == 0 ? n * n : max;
     }
 
-    private int dfs(int i, int j, int color) {
-        if (!isValid(i, j) || grid[i][j] != 1) return 0;
-        
-        grid[i][j] = color; // Mark cell with new color
-        int size = 1;
-        
-        for (int[] dir : DIRECTIONS) {
-            size += dfs(i + dir[0], j + dir[1], color);
+    private int dfs(int[][] grid, int i, int j, int id) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
+            return 0;
         }
-        
-        return size;
-    }
-
-    private boolean isValid(int i, int j) {
-        return i >= 0 && i < n && j >= 0 && j < n;
+        grid[i][j] = id;
+        int count = 1;
+        for (int[] dir : dirs) {
+            count += dfs(grid, i + dir[0], j + dir[1], id);
+        }
+        return count;
     }
 }
