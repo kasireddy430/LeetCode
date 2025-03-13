573fca8b4c82551262fc8c5e61abc2d03da47d7e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-12 19:03:14 -0700

    Time: 36 ms (99.66%), Space: 58.1 MB (5.87%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,74 +1,73 @@
 class Solution {
-
-    private final PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
-    private final PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    private final HashMap<Integer, Integer> map = new HashMap<>();
-    private int balance;
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> Integer.compare(b, a));
+    private Map<Integer, Integer> map = new HashMap<>();
+    private int balance = 0;
 
     public double[] medianSlidingWindow(int[] nums, int k) {
         int n = nums.length;
-        var res = new double[n - k + 1];
+        double[] res = new double[n - k + 1];
 
-        for (int i = 0; i < k; i++) {
+        for(int i = 0; i < k; i++){
             addNum(nums[i]);
         }
 
         res[0] = findMedian();
 
-        for (int i = k; i < n; i++) {
+        for(int i = k; i < n; i++){
             removeNum(nums[i - k]);
             addNum(nums[i]);
-
             res[i - k + 1] = findMedian();
         }
-
-        return res;
+        return res;    
     }
 
-    private void addNum(int num) {
-        if (maxHeap.isEmpty() || maxHeap.peek() >= num) {
+    private void addNum(int num){
+        if(maxHeap.isEmpty() || maxHeap.peek() >= num){
             maxHeap.offer(num);
             balance++;
-        } else {
+        } else{
             minHeap.offer(num);
             balance--;
         }
     }
 
-    private void removeNum(int num) {
+    private void removeNum(int num){
         map.merge(num, 1, Integer::sum);
 
-        if (maxHeap.peek() >= num) {
+        if(maxHeap.peek() >= num){
             balance--;
-        } else {
+        } else{
             balance++;
         }
-    }
+    }    
 
-    private double findMedian() {
+    private double findMedian(){
         balance();
-        cleanHeap(maxHeap);
-        cleanHeap(minHeap);
+        cleanUp(maxHeap);
+        cleanUp(minHeap);
 
-        // balance();
-
-        return balance > 0 ? maxHeap.peek() : ((long) maxHeap.peek() + minHeap.peek()) / 2.0;
+        return balance > 0 ? maxHeap.peek() : ((long)maxHeap.peek() + minHeap.peek())/2.0; 
     }
 
-    private void cleanHeap(PriorityQueue<Integer> heap) {
-        while (map.containsKey(heap.peek())) {
-            map.merge(heap.poll(), 1, (curr, diff) -> curr != 1 ? curr - diff : null);
+    private void cleanUp(PriorityQueue<Integer> heap){
+        while(map.containsKey(heap.peek())){
+            map.merge(heap.poll(), 1, (cur, diff) -> cur != 1 ? cur - diff : null);
         }
     }
 
-    private void balance() {
-        while (balance > 1) {
+    private void balance(){
+        while(balance > 1){
             minHeap.offer(maxHeap.poll());
             balance -= 2;
-        }
-        while (balance < 0) {
+        } 
+
+        while(balance < 0){
             maxHeap.offer(minHeap.poll());
             balance += 2;
         }
     }
 }
+
+//Time Complexity: O()
+//Space Complexity: O(k)
