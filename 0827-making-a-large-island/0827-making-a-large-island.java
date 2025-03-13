83158e722cfb5e076d271f2550fe6cfddd7744cc
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-12 21:01:00 -0700

    Time: 56 ms (83.53%), Space: 73.9 MB (77.28%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.java
+++ 0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,14 +1,15 @@
 public class Solution {
-    private static final int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public int largestIsland(int[][] grid) {
         int n = grid.length;
         List<Integer> key = new ArrayList<>();
         int id = 2;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
                     int size = dfs(grid, i, j, id);
                     key.add(size);
                     id++;
@@ -16,23 +17,24 @@ public class Solution {
             }
         }
 
-        if (key.isEmpty()) return 1;
+        if(key.isEmpty()) return 1;
 
         int max = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0) {
-                    Set<Integer> seen = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0){
                     int sum = 1;
-                    for (int[] dir : dirs) {
-                        int ni = i + dir[0];
-                        int nj = j + dir[1];
-                        if (ni >= 0 && ni < n && nj >= 0 && nj < n && grid[ni][nj] >= 2) {
-                            int islandId = grid[ni][nj];
-                            if (!seen.contains(islandId)) {
-                                sum += key.get(islandId - 2);
-                                seen.add(islandId);
+                    Set<Integer> seen = new HashSet<>();
+
+                    for(int[] dir : directions){
+                        int r = i + dir[0];
+                        int c = j + dir[1];
+
+                        if(r >= 0 && r < n && c >= 0 && c < n && grid[r][c] >= 2){
+                            int currentIsland = grid[r][c];
+                            if(!seen.contains(currentIsland)){
+                                sum += key.get(currentIsland - 2);
+                                seen.add(currentIsland);
                             }
                         }
                     }
@@ -41,18 +43,25 @@ public class Solution {
             }
         }
 
-        return max == 0 ? n * n : max;
+        return max == 0 ? n * n : max;//if all elements are 1 
     }
 
-    private int dfs(int[][] grid, int i, int j, int id) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid.length || grid[i][j] != 1) {
+    private int dfs(int[][] grid, int i, int j, int id){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid.length || grid[i][j] != 1){
             return 0;
         }
+
         grid[i][j] = id;
         int count = 1;
-        for (int[] dir : dirs) {
-            count += dfs(grid, i + dir[0], j + dir[1], id);
+        for(int[] dir : directions){
+            int row = i + dir[0];
+            int col = j + dir[1];
+            count += dfs(grid, row, col, id);
         }
+
         return count;
     }
 }
+
+//Time Complexity: O(n^2)
+//Space Complexity: O(n^2)
