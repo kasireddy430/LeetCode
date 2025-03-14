58d49ff29a947739969761157a3f0d089e98e80b
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-14 03:53:42 -0700

    Time: 45 ms (96.39%), Space: 46.4 MB (36.06%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -1,64 +1,76 @@
-class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean isWord;
-
-    public TrieNode() {
-        children = new HashMap<>();
-        isWord = false;
-    }
-
-    public void addWord(String word) {
-        TrieNode cur = this;
-        for (char c : word.toCharArray()) {
-            cur.children.putIfAbsent(c, new TrieNode());
-            cur = cur.children.get(c);
-        }
-        cur.isWord = true;
-    }
-}
-
 class Solution {
-    private Set<String> res;
-    private boolean[][] visit;
-    
-    public List<String> findWords(char[][] board, String[] words) {
-        TrieNode root = new TrieNode();
-        for (String word : words) {
-            root.addWord(word);
-        }
-
-        int ROWS = board.length, COLS = board[0].length;
-        res = new HashSet<>();
-        visit = new boolean[ROWS][COLS];
-
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                dfs(board, r, c, root, "");
-            }
-        }
-        return new ArrayList<>(res);
+    class Node {
+        Map<Character, Node> children = new HashMap<>();
+        boolean ends = false;
+        String word = null; // Store word at the end node
     }
 
-    private void dfs(char[][] board, int r, int c, TrieNode node, String word) {
-        int ROWS = board.length, COLS = board[0].length;
-        if (r < 0 || c < 0 || r >= ROWS || 
-            c >= COLS || visit[r][c] || 
-            !node.children.containsKey(board[r][c])) {
+    class Trie {
+        Node root;
+        public Trie() {
+            root = new Node();
+        }
+        public void addWord(String word) {
+            Node curr = root;
+            for (char c : word.toCharArray()) {
+                curr.children.putIfAbsent(c, new Node());
+                curr = curr.children.get(c);
+            }
+            curr.ends = true;
+            curr.word = word; // Store full word at the last node
+        }
+    }
+
+    public void dfs(char[][] board, int i, int j, Node node, Set<String> found) {
+        int m = board.length, n = board[0].length;
+
+        // Boundary Check
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] == '#' || !node.children.containsKey(board[i][j])) {
             return;
         }
 
-        visit[r][c] = true;
-        node = node.children.get(board[r][c]);
-        word += board[r][c];
-        if (node.isWord) {
-            res.add(word);
+        char c = board[i][j];
+        Node nextNode = node.children.get(c);
+
+        // If the word is found, add it to the result
+        if (nextNode.ends) {
+            found.add(nextNode.word);
+            nextNode.ends = false; // Prevent duplicate work
         }
 
-        dfs(board, r + 1, c, node, word);
-        dfs(board, r - 1, c, node, word);
-        dfs(board, r, c + 1, node, word);
-        dfs(board, r, c - 1, node, word);
+        // Mark as visited
+        board[i][j] = '#';
 
-        visit[r][c] = false;
+        // Explore 4 directions
+        dfs(board, i + 1, j, nextNode, found);
+        dfs(board, i - 1, j, nextNode, found);
+        dfs(board, i, j + 1, nextNode, found);
+        dfs(board, i, j - 1, nextNode, found);
+
+        // Restore original value
+        board[i][j] = c;
+
+        // **Optimization:** If no words remain in this Trie branch, remove it
+        if (nextNode.children.isEmpty()) {
+            node.children.remove(c);
+        }
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.addWord(word);
+        }
+        Node root = trie.root;
+        Set<String> found = new HashSet<>();
+        int m = board.length, n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, i, j, root, found);
+            }
+        }
+
+        return new ArrayList<>(found);
     }
 }
