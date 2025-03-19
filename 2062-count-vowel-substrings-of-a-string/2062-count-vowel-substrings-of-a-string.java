c22718a472c5e3186fbe5707c32742c1a73ac8b9
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-19 16:41:00 -0700

    Time: 0 ms (100%), Space: 41.5 MB (88.58%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
@@ -1,20 +1,56 @@
-public class Solution {
-    private boolean isVowel(char c) {
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
+class Solution {
+    public int countVowelSubstrings(String word) {
+        int length = word.length();
 
-    public int countVowelSubstrings(String s) {
-        int ans = 0;
-        int n = s.length();
-        
+        boolean[] vows = new boolean[128];//tracks vowel characters in input string
 
-        for (int i = 0; i < n; ++i) {
-            Map<Character, Integer> cnt = new HashMap<>();
-            for (int j = i; j < n && isVowel(s.charAt(j)); ++j) {
-                cnt.put(s.charAt(j), cnt.getOrDefault(s.charAt(j), 0) + 1);
-                if (cnt.size() == 5) ++ans;
+        vows['a'] = true;
+        vows['e'] = true;
+        vows['i'] = true;
+        vows['o'] = true;
+        vows['u'] = true;
+
+        int[] count = new int[128];//tracks count of vowels
+        int uniqueVows = 0;
+        int currentBegin = 0;
+        int begin = 0;
+        int result = 0;
+
+        for(int i = 0; i < length; i++){
+            char c = word.charAt(i);
+
+            if(vows[c]){
+                count[c]++;
+
+                if(count[c] == 1){
+                    uniqueVows++;
+                }
+
+                //shrink window until all unique vowels are present
+                while(uniqueVows == 5){
+                    uniqueVows -= --count[word.charAt(begin)] == 0 ? 1 : 0;
+                    begin++; 
+                }
+                result += begin - currentBegin;
+                
+            } else {
+                //reset vowel counts
+                if(uniqueVows != 0){
+                    uniqueVows = 0;
+
+                    count['a'] = 0;
+                    count['e'] = 0;
+                    count['i'] = 0;
+                    count['o'] = 0;
+                    count['u'] = 0;
+                }
+                currentBegin = begin = i + 1; 
             }
         }
-        return ans;
+        return result;
     }
 }
+
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
