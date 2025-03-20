552228d7cd31fe99cd88ec57c7a0642ae68c634a
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-19 17:32:37 -0700

    Time: 19 ms (52.49%), Space: 45.6 MB (29.12%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,48 +3,42 @@ class Solution {
         if(t.isEmpty()){
             return "";
         }
-        Map<Character, Integer> sCount = new HashMap<>();
-        Map<Character, Integer> tCount = new HashMap<>();
 
+        Map<Character, Integer> tcount = new HashMap<>();
+        Map<Character, Integer> scount = new HashMap<>();
         int matches = 0;
-        int minWindow = Integer.MAX_VALUE;
         int l = 0, start = 0;
+        int minWindow = Integer.MAX_VALUE;
 
         for(int i = 0; i < t.length(); i++){
-            char tChar = t.charAt(i);
-            tCount.put(tChar, tCount.getOrDefault(tChar, 0) + 1);
+            tcount.put(t.charAt(i), tcount.getOrDefault(t.charAt(i), 0) + 1);
         }
 
-        System.out.println(tCount + "tCCCCC");
         for(int r = 0; r < s.length(); r++){
-            char sChar = s.charAt(r);
-            sCount.put(sChar, sCount.getOrDefault(sChar, 0) + 1);
+            char c = s.charAt(r);
+            scount.put(s.charAt(r), scount.getOrDefault(s.charAt(r), 0) + 1);
 
-            System.out.println(sCount + "sCCCCC");
-            if(tCount.containsKey(sChar) && tCount.get(sChar) == sCount.get(sChar)){
+            if(tcount.containsKey(c) && scount.get(c).equals(tcount.get(c))){
                 matches++;
             }
-            System.out.println(matches + "hi");
-            while(matches == t.length()){
+
+            while(matches == tcount.size()){
+
                 if(r - l + 1 < minWindow){
                     minWindow = r - l + 1;
                     start = l;
                 }
 
-                //Shrink window
                 char shrink = s.charAt(l);
-                sCount.put(shrink, sCount.getOrDefault(shrink, 0) - 1);
+                scount.put(shrink, scount.get(shrink) - 1);
 
-                if(tCount.containsKey(shrink) && sCount.get(shrink) < tCount.get(shrink)){
+                if(tcount.containsKey(shrink) && scount.get(shrink) < tcount.get(shrink)){
                     matches--;
                 }
                 l++;
             }
         }
 
-        return minWindow == Integer.MAX_VALUE ? "": s.substring(start, start + minWindow);
+        return minWindow == Integer.MAX_VALUE ? "" : s.substring(start, start + minWindow);
     }
 }
-
-//Time Complexity: O(n)
-//Space Complexity: O(n)
