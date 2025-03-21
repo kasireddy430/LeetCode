49b9c97b398a87ee80effc25340e2de071469ba0
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-20 19:26:08 -0700

    Time: 2 ms (100%), Space: 66.6 MB (53.79%) - LeetHub

--- 3155-maximum-number-of-upgradable-servers/3155-maximum-number-of-upgradable-servers.java
+++ 3155-maximum-number-of-upgradable-servers/3155-maximum-number-of-upgradable-servers.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] maxUpgrades(int[] count, int[] upgrade, int[] sell, int[] money) {
-        int n = count.length;
-        int[] result = new int[n];
-        // upgrade k servers, sell count[i] - k servers
-        // (count[i] - k) * sell[i] + money >= k * upgrade[i]
-        // count[i] * sell[i] + money >= k (upgrade[i] + sell[i])
-        for (int i = 0; i < n; i++) {
-            double den = upgrade[i] + sell[i];
-            double numerator = ((double) count[i] * sell[i] / den) + ((double) money[i] / den);
-            result[i] = Math.min(count[i], (int) numerator);
+        int length = count.length;
+        int[] result = new int[length];
+
+        //If we upgrade k servers then we sell count[i] - k servers 
+        //and add the initial money to the servers we are selling
+        //(count[i] - k) * sell[i] + money[i] >= k * upgrade[i]
+        for(int i = 0; i < length; i++){
+            int den = upgrade[i] + sell[i];
+
+            double k = (double) count[i] * sell[i]/den + (double) money[i]/den;
+
+            result[i] = Math.min(count[i], (int)k);
         }
         return result;
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
