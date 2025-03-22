957047ad8789af1e38064ccc7159bba867be92dc
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-21 19:00:01 -0700

    Time: 3 ms (41.82%), Space: 41.8 MB (65.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                if(!stack.isEmpty() && stack.peek() == map.get(c)){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
