86891caae471800bfecc53c7a17fb2b6719ae44d
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-22 15:19:09 -0700

    Time: 0 ms (100%), Space: 45.2 MB (5.48%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -23,20 +23,25 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        // Base case: If the tree is empty, return null
-        if (root == null) return null;
-        
-        // Connect the left child to the right child
-        if (root.left != null) root.left.next = root.right;
-        
-        // Connect the right child to the left child of the next node, if it exists
-        if (root.right != null && root.next != null) root.right.next = root.next.left;
-        
-        // Recursively connect the left and right subtrees
+        if(root == null){
+            return null;
+        }
+
+        if(root.left != null){
+            root.left.next = root.right;
+        }
+
+        if(root.right != null && root.next != null){
+            root.right.next = root.next.left;
+        }
+
         connect(root.left);
         connect(root.right);
-        
-        // Return the root after connections are made
+
         return root;
     }
 }
+
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
