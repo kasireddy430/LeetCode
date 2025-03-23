3db67e456c98c0634be1a9cf4bb565b52e975d95
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-23 16:09:48 -0700

    Time: 759 ms (28.24%), Space: 111.6 MB (30.53%) - LeetHub

---
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        Integer[][][] dp = new Integer[nums.length+1][k + 1][nums.length + 1];
+        return rec(nums,k,0,-1,dp);
+    }
+    public int rec(int[] nums,int k,int i,int prev,Integer dp[][][]){
+        if(i==nums.length)return dp[i][k][prev+1]= 0;
+        if(dp[i][k][prev+1] !=null)return dp[i][k][prev+1];
+        int take=0;
+        if (prev == -1 || nums[prev] == nums[i]) {            
+            take = 1 + rec(nums, k, i + 1, i,dp);
+        } else if (k > 0) {            
+            take = 1 + rec(nums, k - 1, i + 1, i,dp);
+        }
+        
+        int ntake=rec(nums,k,i+1,prev,dp);
+        return dp[i][k][prev+1]= Math.max(take,ntake);
+    }
+}
