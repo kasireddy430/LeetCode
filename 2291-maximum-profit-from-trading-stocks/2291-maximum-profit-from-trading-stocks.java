5bc6f40afa95b5aae6b34743bfbdc5fbe01a8c6b
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-22 17:12:21 -0700

    Time: 7 ms (99.16%), Space: 43 MB (89.96%) - LeetHub

--- 2291-maximum-profit-from-trading-stocks/2291-maximum-profit-from-trading-stocks.java
+++ 2291-maximum-profit-from-trading-stocks/2291-maximum-profit-from-trading-stocks.java
@@ -2,20 +2,15 @@ class Solution {
     public int maximumProfit(int[] present, int[] future, int budget) {
         int n = present.length;
         int[] dp = new int[budget + 1];
-
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int cost = present[i];
             int profit = future[i] - present[i];
-
-            if(profit < 0) continue;
-
-            for(int j = budget; j >= cost; j--){
+            if (profit <= 0) continue;
+            for (int j = budget; j >= cost; j--) { // it has to go from higher to lower, so that the current value won't be calculated twice
                 dp[j] = Math.max(dp[j], profit + dp[j - cost]);
             }
         }
         return dp[budget];
+        
     }
 }
-
-//Time Complexity: O(n * budget)
-//Space Complexity: O(budget)
