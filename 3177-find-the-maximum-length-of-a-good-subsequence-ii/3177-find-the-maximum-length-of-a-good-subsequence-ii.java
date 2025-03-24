de7b9a7a6f55eba4279abc407e08a8a3e756deff
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-23 20:22:07 -0700

    Time: 10 ms (97.01%), Space: 46 MB (91.04%) - LeetHub

--- 3177-find-the-maximum-length-of-a-good-subsequence-ii/3177-find-the-maximum-length-of-a-good-subsequence-ii.java
+++ 3177-find-the-maximum-length-of-a-good-subsequence-ii/3177-find-the-maximum-length-of-a-good-subsequence-ii.java
@@ -1,37 +1,25 @@
 class Solution {
-    public int maximumLength(int[] arr, int K) {
-        int dp[][] = new int[arr.length][K + 1];
-        
-        int prevmax[] = new int[arr.length]; // stores max previous dp value (for k - 1) starting from i to end
-        for (int k = 0; k <= K; k++) {
-            Map<Integer, Integer> same = new HashMap<>(); //stores [element => max dp value]
-            int curmax[] = new int[arr.length]; //this is used to store max dp value (for k) starting from i to end, this will later be assigned to prevmax[]
-            for (int i = arr.length - 1; i >= 0; i--) {
-                dp[i][k] = 1;
-                
-                //fetch from hash map
-                dp[i][k] = Math.max(
-                    dp[i][k],
-                    Math.max(
-                        1 + same.getOrDefault(arr[i], 0),
-                        (((i + 1) < arr.length) ? (1 + prevmax[i + 1]) : 1)
-                    )                  
-               );
-                
-                //save to hash map
-                final int curdp = dp[i][k];
-                same.compute(arr[i], (key, val) -> {
-                    if (val == null) val = 0;
-                    if (val < curdp) return curdp;
-                    return val;
-                });
-                curmax[i] = Math.max(i + 1 < curmax.length ? curmax[i + 1] : 0, dp[i][k]);
+    public int maximumLength(int[] nums, int k) {
+        Map<Integer, int[]> dp = new HashMap<>();
+        int[] max = new int[k + 2];
+
+        for(int num : nums){
+            int[] len = dp.get(num);
+
+            if(len == null){
+                len = new int[k + 1];
+                dp.put(num, len);
             }
-            prevmax = curmax;
+
+            for(int j = k; j >= 0; j--){
+                len[j] = Math.max(len[j], max[j]) + 1;
+                max[j + 1] = Math.max(max[j + 1], len[j]);
+            }
         }
-        int ans = 0;
-        for (int i = 0; i < arr.length; i++)
-            ans = Math.max(ans, dp[i][K]);
-        return ans;
+        return max[k + 1];
     }
 }
+
+
+//Time Complexity: O(n * k)
+//Space Complexity: O(n * k)
