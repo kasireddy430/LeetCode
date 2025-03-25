22a8f3c600370136245961aed19cec7a483d9716
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-24 20:06:17 -0700

    Time: 5 ms (37.83%), Space: 45.2 MB (25.67%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,34 +1,29 @@
 class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
 
-    private Stack<Integer> stack; // main stack to store values
-    private Stack<Integer> minStack; // auxilary minStack to store minmum values
-
-    //Initialize both the stacks
     public MinStack() {
         stack = new Stack<>();
         minStack = new Stack<>();
     }
     
     public void push(int val) {
-        // add the value to top of the stack
         stack.push(val);
-        // Check if minStack has either no values or current value is less than the existing top value
+
         if(minStack.isEmpty() || val <= minStack.peek()){
-            //If so add it to top of minStack
             minStack.push(val);
         }
     }
     
     public void pop() {
-        if(stack.isEmpty())
+        if(stack.isEmpty()){
             return;
-        // Remove the top element from stack it the stack isn't empty
+        }
+
         int top = stack.pop();
-        // Check if poped element is the minimum value
         if(top == minStack.peek()){
-            //If so remove it from auxilary stack
             minStack.pop();
-        }        
+        }
     }
     
     public int top() {
@@ -39,10 +34,6 @@ class MinStack {
         return minStack.peek();
     }
 }
-
-// Time Complexity: O(1) for all stack operations
-// Space Complexity: O(n) where n is number of elements in stack
-
 
 // Time Complexity: O(1) for all stack operations
 // Space Complexity: O(n) where n is number of elements in stack
