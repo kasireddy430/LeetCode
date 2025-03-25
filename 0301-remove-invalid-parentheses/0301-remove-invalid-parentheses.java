f175b39835ae2eb1561a376ad3ddc77b3099048e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-24 22:42:42 -0700

    Time: 1 ms (98.96%), Space: 42.3 MB (92.87%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -1,24 +1,34 @@
 class Solution {
-public List<String> removeInvalidParentheses(String s) {
-    List<String> ans = new ArrayList<>();
-    remove(s, ans, 0, 0, new char[]{'(', ')'});
-    return ans;
-}
-
-public void remove(String s, List<String> ans, int last_i, int last_j,  char[] par) {
-    for (int stack = 0, i = last_i; i < s.length(); ++i) {
-        if (s.charAt(i) == par[0]) stack++;
-        if (s.charAt(i) == par[1]) stack--;
-        if (stack >= 0) continue;
-        for (int j = last_j; j <= i; ++j)
-            if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
-                remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
-        return;
+    public List<String> removeInvalidParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        remove(s, result, 0, 0, new char[]{'(', ')'});
+        return result;
     }
-    String reversed = new StringBuilder(s).reverse().toString();
-    if (par[0] == '(') // finished left to right
-        remove(reversed, ans, 0, 0, new char[]{')', '('});
-    else // finished right to left
-        ans.add(reversed);
-}
+
+    private void remove(String s, List<String> result, int lastCheckIndex, int lastRemovedIndex, char[] par){
+        int balance = 0;
+
+        for(int i = lastCheckIndex; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == par[0]) balance++;
+            if(c == par[1]) balance--;
+
+            if(balance >= 0) continue;
+
+            for(int j = lastRemovedIndex; j <= i; j++){
+                if(s.charAt(j) == par[1] && (j == lastRemovedIndex || s.charAt(j - 1) != par[1])){
+                    remove(s.substring(0, j) + s.substring(j + 1), result, i, j, par);
+                }
+            }
+            return;
+        }
+
+        String reversed = new StringBuilder(s).reverse().toString();
+
+        if(par[0] == '('){
+            remove(reversed, result, 0, 0, new char[]{')', '('});
+        } else {
+            result.add(reversed);
+        }
+    }
 }
