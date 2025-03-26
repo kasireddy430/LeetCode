62e55fed75b77110def225205d773feb3708a2ca
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-26 14:51:58 -0700

    Time: 36 ms (96%), Space: 46.2 MB (8.53%) - LeetHub

--- 0588-design-in-memory-file-system/0588-design-in-memory-file-system.java
+++ 0588-design-in-memory-file-system/0588-design-in-memory-file-system.java
@@ -1,12 +1,12 @@
-class TrieNode {
+class TrieNode{
     Map<String, TrieNode> children = new TreeMap<>();
-    boolean isFile;
+    boolean isFile = false;
     String content = "";
-    public TrieNode() {}
 }
 
 public class FileSystem {
     TrieNode root;
+
     public FileSystem() {
         root = new TrieNode();
     }
@@ -15,54 +15,64 @@ public class FileSystem {
         TrieNode node = root;
         List<String> res = new ArrayList<>();
         String[] file = path.split("/");
-        for (int i = 1; i < file.length; i++) {
-            String curr = file[i];
-            node = node.children.get(curr);
+
+        for(int i = 1; i < file.length; i++){
+            String cur = file[i];
+            node = node.children.get(cur);
         }
-        if (node.isFile){
-            res.add(file[file.length-1]);
-            return res;
-        }else{
-            for (Map.Entry<String,TrieNode> entry:node.children.entrySet()){
+
+        if(node.isFile){
+            res.add(file[file.length - 1]);
+        } else {
+           for(Map.Entry<String, TrieNode> entry: node.children.entrySet()){
                 res.add(entry.getKey());
-            }
-            return res;
+           }
         }
+        return res;
     }
 
     public void mkdir(String path) {
         TrieNode node = root;
         String[] file = path.split("/");
-        for (int i = 1; i < file.length; i++) {
-            String curr = file[i];
-            node.children.putIfAbsent(curr,new TrieNode());
-            node = node.children.get(curr);
-        }
 
+        for(int i = 1; i < file.length; i++){
+            String cur = file[i];
+            node.children.putIfAbsent(cur, new TrieNode());
+            node = node.children.get(cur);
+        }
     }
 
     public void addContentToFile(String filePath, String content) {
         TrieNode node = root;
         String[] file = filePath.split("/");
-        for (int i = 1; i < file.length; i++) {
-            String curr = file[i];
-            node.children.putIfAbsent(curr,new TrieNode());
-            node = node.children.get(curr);
+
+        for(int i = 1; i < file.length; i++){
+            String cur = file[i];
+            node.children.putIfAbsent(cur, new TrieNode());
+            node = node.children.get(cur);
         }
+
         node.isFile = true;
-        node.content+=content;
+        node.content += content;
     }
 
     public String readContentFromFile(String filePath) {
         TrieNode node = root;
         String[] file = filePath.split("/");
-        for (int i = 1; i < file.length; i++) {
-            String curr = file[i];
-            node = node.children.get(curr);
+
+        for(int i = 1; i < file.length; i++){
+            String cur = file[i];
+            node.children.putIfAbsent(cur, new TrieNode());
+            node = node.children.get(cur);
         }
+
         return node.content;
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+//where n is depth of the path
 
 /**
  * Your FileSystem object will be instantiated and called as such:
