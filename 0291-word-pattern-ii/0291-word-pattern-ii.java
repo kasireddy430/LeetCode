4b141d66d6fc5447c9afec587e4d6a71aa4fd7ed
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-26 16:05:59 -0700

    Time: 1 ms (98.67%), Space: 41.9 MB (20%) - LeetHub

--- 0291-word-pattern-ii/0291-word-pattern-ii.java
+++ 0291-word-pattern-ii/0291-word-pattern-ii.java
@@ -1,36 +1,48 @@
 class Solution {
-    Map<Character, String> map;
-    Set<String> revMap;
     public boolean wordPatternMatch(String pattern, String s) {
-        map = new HashMap<>();
-        revMap = new HashSet<>();
-        return checkForPatter(pattern, s);
+        return backtrack(0, 0, pattern, s, new HashMap<>(), new HashSet<>());
     }
 
-    boolean checkForPatter(String pattern, String s) {
-        if(pattern.isEmpty()) 
-            return s.isEmpty();
-        if(map.containsKey(pattern.charAt(0))) {
-            if(s.startsWith(map.get(pattern.charAt(0)))) {
-                return checkForPatter(pattern.substring(1), 
-                s.substring(map.get(pattern.charAt(0)).length()));
-            }
-            else 
-                return false;
+    private boolean backtrack(int i, int j, String pattern, String s, Map<Character, String> map,
+                            Set<String> vals){
+
+        if(i >= pattern.length() && j >= s.length()){
+            return true;
         }
-        for(int i=0;i<s.length();i++) {
-            if(revMap.contains(s.substring(0,i+1)))
-                continue;
-            revMap.add(s.substring(0,i+1));
-            map.put(pattern.charAt(0), s.substring(0,i+1));
-            if(checkForPatter(pattern.substring(1), s.substring(i+1))) {
+        
+        if(i >= pattern.length() || j >= s.length()){
+            return false;
+        }
+
+        if(map.containsKey(pattern.charAt(i))){
+            String expectedWord = map.get(pattern.charAt(i));
+
+            if(!s.startsWith(expectedWord, j)){
+                return false;
+            }
+
+            return backtrack(i + 1, j + expectedWord.length(), pattern, s, map, vals);
+        }
+
+        for(int k = j; k < s.length(); k++){
+            String value = s.substring(j, k+1);
+            if(vals.contains(value)) continue;
+
+            map.put(pattern.charAt(i), value);
+            vals.add(value);
+
+            //recur for next character in pattern and next substring in s
+            if(backtrack( i + 1, k + 1, pattern, s, map, vals)){
                 return true;
             }
-            revMap.remove(s.substring(0,i+1));
-            map.remove(pattern.charAt(0));
+
+            //backtracking if not found valid
+            map.remove(pattern.charAt(i));
+            vals.remove(value);        
         }
         return false;
     }
-
-
 }
+
+//Time Complexity: O(2 ^ n)
+//Space Complexity: O(n)
