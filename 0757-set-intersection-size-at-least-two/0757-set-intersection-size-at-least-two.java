e57832936fbbe7c653475eddda739fd38cf4be99
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-25 23:20:45 -0700

    Time: 10 ms (36.36%), Space: 44.9 MB (87.88%) - LeetHub

--- 0757-set-intersection-size-at-least-two/0757-set-intersection-size-at-least-two.java
+++ 0757-set-intersection-size-at-least-two/0757-set-intersection-size-at-least-two.java
@@ -1,21 +1,30 @@
 class Solution {
     public int intersectionSizeTwo(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) ->
-                    a[0] != b[0] ? a[0]-b[0] : b[1]-a[1]);
-        int[] todo = new int[intervals.length];
-        Arrays.fill(todo, 2);
-        int ans = 0, t = intervals.length;
-        while (--t >= 0) {
-            int s = intervals[t][0];
-            int e = intervals[t][1];
-            int m = todo[t];
-            for (int p = s; p < s+m; ++p) {
-                for (int i = 0; i <= t; ++i)
-                    if (todo[i] > 0 && p <= intervals[i][1])
-                        todo[i]--;
-                ans++;
+        Arrays.sort(intervals, (a, b) -> a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]);
+
+        int point1 = -1, point2 = -1;
+        int count = 0;
+
+        for(int[] interval : intervals){
+            if(point1 == -1){
+                count += 2;
+                point1 = interval[1] - 1;
+                point2 = interval[1];
+            } else if(interval[0] <= point1){
+                continue;
+            } else if(interval[0] <= point2){
+                point1 = point2;
+                point2 = interval[1];
+                count++;
+            } else{
+                count += 2;
+                point1 = interval[1] - 1;
+                point2 = interval[1];                
             }
         }
-        return ans;
+        return count;
     }
 }
+
+//Time Complexity: O(n log n)
+//Space Complexity: O(n). where n is no.of intervals
