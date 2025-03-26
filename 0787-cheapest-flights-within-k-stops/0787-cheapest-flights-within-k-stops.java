2b1172d9776b2923e94c02d1f8ef85725288766e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-25 22:00:22 -0700

    Time: 6 ms (84.71%), Space: 44.5 MB (56.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -5,24 +5,30 @@ class Solution {
         for (int[] flight : flights) {
             adj.get(flight[0]).add(new int[] {flight[1], flight[2]});
         }
+
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0});
+        q.offer(new int[] {src, 0, 0}); // {node, cost, stops}
+
         int[] minCost = new int[n];
         Arrays.fill(minCost, Integer.MAX_VALUE);
-        int stops = 0;
-        while (!q.isEmpty() && stops <= k) {
-            int size = q.size();
-            while (size-- > 0) {
-                int[] curr = q.poll();
-                for (int[] neighbour : adj.get(curr[0])) {
-                    int price = neighbour[1], neighbourNode = neighbour[0];
-                    if (price + curr[1] >= minCost[neighbourNode]) continue;
-                    minCost[neighbourNode] = price + curr[1];
-                    q.offer(new int[] {neighbourNode, minCost[neighbourNode]});
+        minCost[src] = 0;
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int node = curr[0], cost = curr[1], stops = curr[2];
+
+            if (stops > k) continue;
+
+            for (int[] neighbor : adj.get(node)) {
+                int nextNode = neighbor[0], price = neighbor[1];
+
+                if (cost + price < minCost[nextNode]) {
+                    minCost[nextNode] = cost + price;
+                    q.offer(new int[] {nextNode, minCost[nextNode], stops + 1});
                 }
             }
-            stops++;
         }
+
         return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
     }
 }
