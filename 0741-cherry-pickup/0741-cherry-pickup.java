664d0fa998491888653c3fe03f7d3f16b262e9c9
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-27 16:23:22 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,40 +1,37 @@
 public class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
-        Integer[][][] memo = new Integer[n][n][n];
-        return Math.max(0, dp(grid, memo, 0, 0, 0));
+        Integer[][] memo = new Integer[n][n];  // Only need to track one person's position
+        return Math.max(0, dp(grid, memo, 0, 0));  // Start from (0, 0)
     }
     
-    private int dp(int[][] grid, Integer[][][] memo, int r1, int c1, int c2) {
+    private int dp(int[][] grid, Integer[][] memo, int r, int c) {
         int n = grid.length;
-        int r2 = r1 + c1 - c2;
         
-        // Out of bounds or thorn cell
-        if (r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
+        // If out of bounds or thorn cell (-1)
+        if (r >= n || c >= n || grid[r][c] == -1) {
             return Integer.MIN_VALUE;
+        }
         
-        // Memoization check
-        if (memo[r1][c1][c2] != null)
-            return memo[r1][c1][c2];
+        // Memoization check to return already computed result
+        if (memo[r][c] != null) {
+            return memo[r][c];
+        }
         
-        // Base case: reached bottom-right corner
-        if (r1 == n - 1 && c1 == n - 1)
-            return grid[r1][c1];
-        
-        // Calculate cherries collected at (r1, c1) and (r2, c2)
-        int cherries = (r1 == r2 && c1 == c2) ? grid[r1][c1] : grid[r1][c1] + grid[r2][c2];
+        // Base case: reached bottom-right corner (goal)
+        if (r == n - 1 && c == n - 1) {
+            return grid[r][c];
+        }
         
         // Move right or down
-        int maxCherries = Math.max(dp(grid, memo, r1, c1 + 1, c2), dp(grid, memo, r1 + 1, c1, c2));
-        // Move down or right
-        maxCherries = Math.max(maxCherries, dp(grid, memo, r1, c1 + 1, c2 + 1));
-        maxCherries = Math.max(maxCherries, dp(grid, memo, r1 + 1, c1, c2 + 1));
+        int maxCherries = Math.max(dp(grid, memo, r + 1, c), dp(grid, memo, r, c + 1));
         
-        // Memoize the result
-        memo[r1][c1][c2] = (maxCherries == Integer.MIN_VALUE) ? Integer.MIN_VALUE : maxCherries + cherries;
+        // Add current cell's cherries
+        maxCherries = (maxCherries == Integer.MIN_VALUE) ? Integer.MIN_VALUE : maxCherries + grid[r][c];
         
-        return memo[r1][c1][c2];
+        // Memoize and return the result
+        memo[r][c] = maxCherries;
+        
+        return memo[r][c];
     }
-
-  
 }
