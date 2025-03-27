57322b1eae9da2091fb5614b6f7da7eec64c5553
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-27 16:23:37 -0700

    Time: 13 ms (98.3%), Space: 44.8 MB (88.68%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,37 +1,38 @@
 public class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
-        Integer[][] memo = new Integer[n][n];  // Only need to track one person's position
-        return Math.max(0, dp(grid, memo, 0, 0));  // Start from (0, 0)
+
+        Integer[][][] memo = new Integer[n][n][n];
+        return Math.max(0, dp(grid, memo, 0, 0, 0));
     }
-    
-    private int dp(int[][] grid, Integer[][] memo, int r, int c) {
+
+    private int dp(int[][] grid, Integer[][][] memo, int r1, int c1, int c2){
         int n = grid.length;
-        
-        // If out of bounds or thorn cell (-1)
-        if (r >= n || c >= n || grid[r][c] == -1) {
+        int r2 = r1 + c1 - c2;
+
+        if(r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1){
             return Integer.MIN_VALUE;
         }
-        
-        // Memoization check to return already computed result
-        if (memo[r][c] != null) {
-            return memo[r][c];
+
+        if(memo[r1][c1][c2] != null){
+            return memo[r1][c1][c2];
         }
-        
-        // Base case: reached bottom-right corner (goal)
-        if (r == n - 1 && c == n - 1) {
-            return grid[r][c];
+
+        if(r1 == n - 1 && c1 == n - 1){
+            return grid[r1][c1];
         }
-        
-        // Move right or down
-        int maxCherries = Math.max(dp(grid, memo, r + 1, c), dp(grid, memo, r, c + 1));
-        
-        // Add current cell's cherries
-        maxCherries = (maxCherries == Integer.MIN_VALUE) ? Integer.MIN_VALUE : maxCherries + grid[r][c];
-        
-        // Memoize and return the result
-        memo[r][c] = maxCherries;
-        
-        return memo[r][c];
+
+        int cherry = (r1 == r2 && c1 == c2) ? grid[r1][c1] : grid[r1][c1] + grid[r2][c2];
+
+        int maxCherry = Math.max(dp(grid, memo, r1 + 1, c1, c2), dp(grid, memo, r1, c1 + 1, c2));
+        maxCherry = Math.max(maxCherry, dp(grid, memo, r1 + 1, c1, c2 + 1));
+        maxCherry = Math.max(maxCherry, dp(grid, memo, r1, c1 + 1, c2 + 1));
+
+        memo[r1][c1][c2] = maxCherry == Integer.MIN_VALUE ? Integer.MIN_VALUE : maxCherry + cherry;
+
+        return memo[r1][c1][c2];
     }
 }
+
+//Time Complexity:
+//Space Complexity:
