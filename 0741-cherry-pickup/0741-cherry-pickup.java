8de45aa05d7e4f1538dde8c2f3810b896754048c
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-27 15:03:19 -0700

    Time: 13 ms (98.3%), Space: 45.2 MB (45.28%) - LeetHub

--- 0741-cherry-pickup/0741-cherry-pickup.java
+++ 0741-cherry-pickup/0741-cherry-pickup.java
@@ -1,33 +1,40 @@
-class Solution {
+public class Solution {
     public int cherryPickup(int[][] grid) {
         int n = grid.length;
-        if(n == 1) return Math.max(0, grid[0][0]);
-        int[][][] dp = new int[50][50][50]; // 1
-        
-        dfs(grid, 0, 0, 0, dp, n); 
-        
-        return Math.max(0, dp[0][0][0]); // 2
+        Integer[][][] memo = new Integer[n][n][n];
+        return Math.max(0, dp(grid, memo, 0, 0, 0));
     }
     
-    
-    int dfs(int[][] arr, int r1, int c1, int c2, int[][][] dp, int n){
-        int r2 = r1+c1-c2; // 3
+    private int dp(int[][] grid, Integer[][][] memo, int r1, int c1, int c2) {
+        int n = grid.length;
+        int r2 = r1 + c1 - c2;
         
-        if(r1>=n || r2>=n || c1>=n || c2>=n || arr[r1][c1]==-1 || arr[r2][c2] == -1) 
+        // Out of bounds or thorn cell
+        if (r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
             return Integer.MIN_VALUE;
         
-        if(dp[r1][c1][c2]!= 0) return dp[r1][c1][c2];
+        // Memoization check
+        if (memo[r1][c1][c2] != null)
+            return memo[r1][c1][c2];
         
-        if(r1 == n-1 && c1 == n-1) return arr[n-1][n-1];
+        // Base case: reached bottom-right corner
+        if (r1 == n - 1 && c1 == n - 1)
+            return grid[r1][c1];
         
-        int ans = arr[r1][c1];
-        // same cell
-        if(c1 != c2) ans+=arr[r2][c2];
+        // Calculate cherries collected at (r1, c1) and (r2, c2)
+        int cherries = (r1 == r2 && c1 == c2) ? grid[r1][c1] : grid[r1][c1] + grid[r2][c2];
         
-        int dd = dfs(arr, r1+1, c1, c2, dp, n);
-        int rd = dfs(arr, r1, c1+1, c2, dp, n);
-        int dr = dfs(arr, r1+1, c1, c2+1, dp, n);
-        int rr = dfs(arr, r1, c1+1, c2+1, dp, n);
-        return dp[r1][c1][c2] = Math.max(dd, Math.max(rd, Math.max(dr, rr)))+ans;
+        // Move right or down
+        int maxCherries = Math.max(dp(grid, memo, r1, c1 + 1, c2), dp(grid, memo, r1 + 1, c1, c2));
+        // Move down or right
+        maxCherries = Math.max(maxCherries, dp(grid, memo, r1, c1 + 1, c2 + 1));
+        maxCherries = Math.max(maxCherries, dp(grid, memo, r1 + 1, c1, c2 + 1));
+        
+        // Memoize the result
+        memo[r1][c1][c2] = (maxCherries == Integer.MIN_VALUE) ? Integer.MIN_VALUE : maxCherries + cherries;
+        
+        return memo[r1][c1][c2];
     }
+
+  
 }
