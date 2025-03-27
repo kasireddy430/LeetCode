8deea02dd066d69bdc716f0395ddbb7a40780d7b
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-26 17:09:36 -0700

    Time: 0 ms (100%), Space: 44.2 MB (39.09%) - LeetHub

--- 0330-patching-array/0330-patching-array.java
+++ 0330-patching-array/0330-patching-array.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minPatches(int[] nums, int n) {
+        int i = 0;
         long missing = 1;
         int patches = 0;
-        int index = 0;
 
-        while (missing <= n) {
-            if (index < nums.length && nums[index] <= missing) {
-                missing += nums[index];
-                index++;
-            } else {
+        while(missing <= n){
+            if(i < nums.length && nums[i] <= missing){
+                missing += nums[i];
+                i++;
+            } else{
                 missing += missing;
                 patches++;
             }
         }
-
         return patches;
     }
 }
+
+//Time Complexity: O(n)
+//Space COmplexity: O(1)
