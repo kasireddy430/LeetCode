e4a606136e04f5d46d42efe594f6311650a42319
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-27 11:16:15 -0700

    Time: 9 ms (89.93%), Space: 45 MB (85.23%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,19 +1,11 @@
 class Solution {
-    public int paintWalls(int[] cost, int[] time) {
-        int n=cost.length;
-        return (int)paintWallsHelper(cost,time,0,0,new Long[n][501]);
-    }
-    
-    private long paintWallsHelper(int[] cost, int[] time, int index, int total, Long[][] memo) {
-        if(total >= cost.length)
-            return 0;
-        if(index >= cost.length)
-            return Integer.MAX_VALUE;
-        if(memo[index][total] != null)
-            return memo[index][total];
-        
-        long with=cost[index] + paintWallsHelper(cost,time,index+1,total+time[index]+1,memo);
-        long without=paintWallsHelper(cost,time,index+1,total,memo);
-        return memo[index][total]=Math.min(with,without);
+        public int paintWalls(int[] cost, int[] time) {
+        int n = cost.length, dp[] = new int[n + 1];
+        Arrays.fill(dp, (int)1e9);
+        dp[0] = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = n; j > 0; --j)
+                dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
+        return dp[n];
     }
 }
