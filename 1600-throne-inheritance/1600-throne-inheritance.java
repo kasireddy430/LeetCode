158713cecabc7a25d874d409bd9284d485e2e06e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-28 16:34:53 -0700

    Time: 233 ms (98.23%), Space: 101.7 MB (95.58%) - LeetHub

--- 1600-throne-inheritance/1600-throne-inheritance.java
+++ 1600-throne-inheritance/1600-throne-inheritance.java
@@ -50,3 +50,11 @@ class ThroneInheritance {
     }
 
 }
+
+/**
+ * Your ThroneInheritance object will be instantiated and called as such:
+ * ThroneInheritance obj = new ThroneInheritance(kingName);
+ * obj.birth(parentName,childName);
+ * obj.death(name);
+ * List<String> param_3 = obj.getInheritanceOrder();
+ */
