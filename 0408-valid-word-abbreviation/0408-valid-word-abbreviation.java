b6a2aa6a3cb6afdb6a9669d9048359b47007f0c1
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-28 15:16:02 -0700

    Time: 1 ms (96.42%), Space: 41.7 MB (79.74%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,29 +1,57 @@
+/**
+ * Time Complexity: O(A)
+ *
+ * Space Complexity: O(1)
+ *
+ * A = Length of abbr input string.
+ */
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        if (word.isEmpty() != abbr.isEmpty()) {
+        if (word == null || abbr == null) {
+            throw new IllegalArgumentException("Input is null");
+        }
+
+        int wLen = word.length();
+        int aLen = abbr.length();
+
+        // length of abbreviation cannot be greater than word's length
+        if (aLen > wLen) {
             return false;
         }
 
-        if (word.isEmpty()) return true;
-
-        if (word.charAt(0) == abbr.charAt(0)) {
-            return validWordAbbreviation(word.substring(1), abbr.substring(1));
+        if (wLen == 0) {
+            return true;
         }
 
-        if (abbr.charAt(0) == '0' || !Character.isDigit(abbr.charAt(0))) {
-            return false;
+        int i = 0;
+        int j = 0;
+
+        while (i < wLen && j < aLen) {
+            // It current characters in both word and abbr is same continue checking.
+            if (word.charAt(i) == abbr.charAt(j)) {
+                i++;
+                j++;
+                continue;
+            }
+
+            // Now current characters in word and abbr do not match. Thus current character
+            // in abbr should be a valid starting digit 0 < x <= 9.
+            if (abbr.charAt(j) == '0' || !Character.isDigit(abbr.charAt(j))) {
+                return false;
+            }
+
+            // The num value
+            int num = 0;
+            while (j < aLen && Character.isDigit(abbr.charAt(j))) {
+                num = 10 * num + (abbr.charAt(j) - '0');
+                j++;
+            }
+
+            // Increment word pinter by num.
+            i += num;
         }
 
-        int count = 0, index = 0;
-
-        while (index < abbr.length() && Character.isDigit(abbr.charAt(index))) {
-            count = 10 * count + (abbr.charAt(index) - '0');
-            index++;
-        }
-
-        if (count > word.length()) return false;
-
-        return validWordAbbreviation(word.substring(count), abbr.substring(index));
-
+        // If both i and j pointers are at end, then we have a valid word abbreviation
+        return i == wLen && j == aLen;
     }
 }
