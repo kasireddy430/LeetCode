215b1f5cbbc79724441efd19cf076af1a450ddb7
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-27 22:53:40 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2702-minimum-operations-to-make-numbers-non-positive/2702-minimum-operations-to-make-numbers-non-positive.java
+++ 2702-minimum-operations-to-make-numbers-non-positive/2702-minimum-operations-to-make-numbers-non-positive.java
@@ -1,31 +1,43 @@
 class Solution {
     public int minOperations(int[] nums, int x, int y) {
         int right = nums[0];
-        for (int v : nums) {
-            right = Math.max(v, right);
+
+        for(int v : nums){
+            right = Math.max(right, v);
         }
+
         int left = 1;
-        right = right / y;
+        right = (right)/y;
+
         x -= y;
-        while (left <= right) {
-            int mid = (left + right) >> 1, s = mid;
-            for (int v : nums) {
-                int t = (v + y - 1) / y;
-                if (mid >= t) {
+
+        while(left <= right){
+            int mid = (left + right)>>1, s = mid;
+
+            for(int v : nums){
+                int t = (v + y - 1)/y;
+
+                if(mid >= t) {
                     continue;
                 }
-                s -= (v - mid * y + x - 1) / x;
-                if (s < 0) {
+
+                s -= (v - mid * y + x - 1)/y;
+
+                if(s < 0){
                     break;
                 } 
             }
-            if (s >= 0) {
+
+            if(s >= 0){
                 right = mid - 1;
-            } else {
+            } else{
                 left = mid + 1;
             }
         }
+
         return right + 1;
-        
     }
 }
+
+//Time Complexity: O(n log m)
+//Space Complexity: O(1)
