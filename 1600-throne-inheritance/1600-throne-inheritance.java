c9c74fc5e6c859785cf2bae402c580939c2bd595
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-28 15:50:06 -0700

    Time: 282 ms (30.97%), Space: 102 MB (93.36%) - LeetHub

--- 1600-throne-inheritance/1600-throne-inheritance.java
+++ 1600-throne-inheritance/1600-throne-inheritance.java
@@ -1,61 +1,52 @@
 class ThroneInheritance {
-    HashMap<String,ArrayList<String>> h;
-    String king="";
-    HashSet<String> hset=new HashSet<String>();
+    String king;
+    TreeNode root;
+    Map<String, TreeNode > graph = new HashMap<>();
+    List<String> ans = new ArrayList<>();
+
     public ThroneInheritance(String kingName) {
-        h=new HashMap<String,ArrayList<String>>();
-        king=kingName;
+        king = kingName;
+        root = new TreeNode(kingName);
+        graph.put(kingName, root);  
     }
-    
+
     public void birth(String parentName, String childName) {
-        if(!h.containsKey(parentName))
-        {
-            ArrayList<String> l=new ArrayList<String>();
-            l.add(childName);
-            h.put(parentName,l);
-        }
-        else
-        {
-             ArrayList<String> l=h.get(parentName);
-            l.add(childName);
-            h.put(parentName,l);
-        }
+     
+        TreeNode child = new TreeNode(childName);
+        graph.put(childName, child);
+        graph.get(parentName).children.add(child);               
     }
-    
+
     public void death(String name) {
-        hset.add(name);
+        TreeNode node = graph.get(name);
+        if (node != null) 
+            node.isDead = true;
     }
-    
+
     public List<String> getInheritanceOrder() {
-        ArrayList<String> result=new ArrayList<String>();
-        Stack<String> stck=new Stack<String>();
-        stck.add(king);
-        while(stck.size()>0)
-        {
-            String s=stck.pop();
-            if(!hset.contains(s))
-            {
-                result.add(s);
-            }
-            if(h.containsKey(s))
-            {
-                ArrayList<String> l=h.get(s);
-                for(int j=l.size()-1;j>=0;j--)
-                {
-                    stck.push(l.get(j));
-                }
-            }
-        }
-
-        return result;
-        
+        ans.clear();
+        dfs(root);
+        return ans;
     }
-}
 
-/**
- * Your ThroneInheritance object will be instantiated and called as such:
- * ThroneInheritance obj = new ThroneInheritance(kingName);
- * obj.birth(parentName,childName);
- * obj.death(name);
- * List<String> param_3 = obj.getInheritanceOrder();
- */
+    private void dfs(TreeNode u) {
+        if (!u.isDead) 
+            ans.add(u.name);
+    
+        for (TreeNode v : u.children) 
+            dfs(v);
+    }
+
+    class TreeNode {
+        String name;
+        List<TreeNode> children;
+        boolean isDead;
+        
+        TreeNode(String name) {
+            this.name = name;
+            this.children = new ArrayList<>();
+            this.isDead = false;
+        }
+    }
+
+}
