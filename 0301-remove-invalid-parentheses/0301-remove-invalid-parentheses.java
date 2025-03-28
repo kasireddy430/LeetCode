9652656766b5733844c6cc84b414e1aba4484b7b
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-27 21:12:39 -0700

    Time: 1 ms (98.98%), Space: 42.4 MB (81.09%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.java
@@ -1,34 +1,37 @@
 class Solution {
     public List<String> removeInvalidParentheses(String s) {
-        List<String> result = new ArrayList<>();
-        remove(s, result, 0, 0, new char[]{'(', ')'});
-        return result;
+        List<String> res = new ArrayList<>();
+        dfs(s, res, 0, 0, new char[]{'(', ')'});
+        return res;
     }
 
-    private void remove(String s, List<String> result, int lastCheckIndex, int lastRemovedIndex, char[] par){
+    private void dfs(String s, List<String> res, int lastCheckIndex, int lastRemoveIndex, char[] par){
         int balance = 0;
 
         for(int i = lastCheckIndex; i < s.length(); i++){
             char c = s.charAt(i);
+
             if(c == par[0]) balance++;
             if(c == par[1]) balance--;
 
             if(balance >= 0) continue;
 
-            for(int j = lastRemovedIndex; j <= i; j++){
-                if(s.charAt(j) == par[1] && (j == lastRemovedIndex || s.charAt(j - 1) != par[1])){
-                    remove(s.substring(0, j) + s.substring(j + 1), result, i, j, par);
+            for(int j = lastRemoveIndex; j <= i; j++){
+                if(s.charAt(j) == par[1] && (j == lastRemoveIndex || s.charAt(j - 1) != par[1])){
+                    dfs(s.substring(0, j) + s.substring(j + 1), res, i, j, par);
                 }
             }
             return;
         }
 
-        String reversed = new StringBuilder(s).reverse().toString();
-
+        String reverse = new StringBuilder(s).reverse().toString();
         if(par[0] == '('){
-            remove(reversed, result, 0, 0, new char[]{')', '('});
+             dfs(reverse, res, 0, 0, new char[]{')', '('});
         } else {
-            result.add(reversed);
+            res.add(reverse);
         }
     }
 }
+
+//Time Complexity: O(2 ^ n)
+//Space Complexity: O(2 ^ n)
