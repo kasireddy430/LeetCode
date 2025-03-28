76949aae1d10fba6993f8d455fdabff7eba12e9e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-28 15:08:13 -0700

    Time: 1 ms (96.42%), Space: 42.3 MB (7.28%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
-        if (word.isEmpty() != abbr.isEmpty()) {
-            return false;
+        int i = 0;
+        int j = 0;
+        
+        while (i < word.length() && j < abbr.length()) {
+            if (Character.isDigit(abbr.charAt(j))) {
+               if (abbr.charAt(j) == '0') {
+                    return false;
+                }
+
+                int num = 0;
+                while (j < abbr.length() && Character.isDigit(abbr.charAt(j))) {
+                    num = num * 10 + (abbr.charAt(j) - '0');
+                    j++;
+                }
+
+                i += num;
+            } else {
+                if (i >= word.length() || word.charAt(i) != abbr.charAt(j)) {
+                    return false;
+                }
+                i++;
+                j++;
+            }
         }
 
-        if (word.isEmpty()) return true;
-
-        if (word.charAt(0) == abbr.charAt(0)) {
-            return validWordAbbreviation(word.substring(1), abbr.substring(1));
-        }
-
-        if (abbr.charAt(0) == '0' || !Character.isDigit(abbr.charAt(0))) {
-            return false;
-        }
-
-        int count = 0, index = 0;
-
-        while (index < abbr.length() && Character.isDigit(abbr.charAt(index))) {
-            count = 10 * count + (abbr.charAt(index) - '0');
-            index++;
-        }
-
-        if (count > word.length()) return false;
-
-        return validWordAbbreviation(word.substring(count), abbr.substring(index));
-
+        return i == word.length() && j == abbr.length();
     }
 }
