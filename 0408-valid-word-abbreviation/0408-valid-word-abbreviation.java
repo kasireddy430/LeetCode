7fef9a85578c70e4849db2dce769c71d51183740
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-28 15:36:07 -0700

    Time: 1 ms (96.42%), Space: 42.1 MB (29.64%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.java
@@ -1,53 +1,42 @@
-/**
- * Time Complexity: O(A)
- *
- * Space Complexity: O(1)
- *
- * A = Length of abbr input string.
- */
 class Solution {
     public boolean validWordAbbreviation(String word, String abbr) {
         int wLen = word.length();
         int aLen = abbr.length();
 
-        // length of abbreviation cannot be greater than word's length
-        if (aLen > wLen) {
+        if(aLen > wLen){
             return false;
         }
 
-        if (wLen == 0) {
-            return true;
-        }
+        
 
-        int i = 0;
-        int j = 0;
-
-        while (i < wLen && j < aLen) {
-            // It current characters in both word and abbr is same continue checking.
-            if (word.charAt(i) == abbr.charAt(j)) {
+        int i = 0, j = 0;
+        while(i < wLen && j < aLen){
+            if(word.charAt(i) == abbr.charAt(j)){
                 i++;
                 j++;
                 continue;
             }
 
-            // Now current characters in word and abbr do not match. Thus current character
-            // in abbr should be a valid starting digit 0 < x <= 9.
-            if (abbr.charAt(j) == '0' || !Character.isDigit(abbr.charAt(j))) {
+            if(abbr.charAt(j) == '0' || !Character.isDigit(abbr.charAt(j))){
                 return false;
             }
 
-            // The num value
             int num = 0;
-            while (j < aLen && Character.isDigit(abbr.charAt(j))) {
-                num = 10 * num + (abbr.charAt(j) - '0');
+
+            while(j < aLen && Character.isDigit(abbr.charAt(j))){
+                num = num * 10 + (abbr.charAt(j) - '0');
                 j++;
             }
 
-            // Increment word pinter by num.
             i += num;
         }
 
-        // If both i and j pointers are at end, then we have a valid word abbreviation
         return i == wLen && j == aLen;
     }
 }
+
+/**
+ * Time Complexity: O(A)
+ * Space Complexity: O(1)
+ * A = Length of abbr input string.
+ */
