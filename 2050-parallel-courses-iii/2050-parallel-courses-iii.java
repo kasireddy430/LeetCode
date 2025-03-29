8f75b697f62c36b4cd851728f08fad1616034798
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-03-28 17:41:55 -0700

    Time: 18 ms (92.42%), Space: 75.2 MB (29.34%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,39 +1,42 @@
-
-
-public class Solution {
+class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
         List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+
+        for(int i = 0; i < n; i++){
             graph.add(new ArrayList<>());
         }
 
-        for (int[] relation : relations) {
+        for(int[] relation : relations){
             int prev = relation[0] - 1;
-            int next = relation[1] - 1;
-            graph.get(next).add(prev);
+            int nxt = relation[1] - 1;
+
+            graph.get(nxt).add(prev);
         }
 
         int[] memo = new int[n];
-        int overallMinTime = 0;
-
-        for (int i = 0; i < n; i++) {
-            overallMinTime = Math.max(overallMinTime, calculateTime(i, graph, time, memo));
+        int overallTime = 0;
+        for(int i = 0; i < n; i++){
+            overallTime = Math.max(overallTime, completeCourse(i, graph, time, memo));
         }
 
-        return overallMinTime;
+        return overallTime;
     }
 
-    private int calculateTime(int course, List<List<Integer> > graph, int[] time, int[] memo) {
-        if (memo[course] != 0) {
-            return memo[course];
+    private int completeCourse(int cur, List<List<Integer>> graph, int[] time, int[] memo){
+        if(memo[cur] != 0){
+            return memo[cur];
         }
 
-        int maxPrerequisiteTime = 0;
-        for (int prereq : graph.get(course)) {
-            maxPrerequisiteTime = Math.max(maxPrerequisiteTime, calculateTime(prereq, graph, time, memo));
+        int coursePreTime = 0;        
+        for(int preReq : graph.get(cur)){
+            coursePreTime = Math.max(coursePreTime, completeCourse(preReq, graph, time, memo));
         }
 
-        memo[course] = maxPrerequisiteTime + time[course];
-        return memo[course];
+        memo[cur] = coursePreTime + time[cur];
+
+        return memo[cur];
     }
 }
+
+//Time Complexity: O(V + E) for graph construction
+//Space Complexity: O(V + E) for storing graph
