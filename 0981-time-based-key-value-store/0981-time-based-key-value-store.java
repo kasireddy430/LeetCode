5a04f5d3aee68f90a3916f5bfdceae074002ef5f
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-04-03 18:39:51 -0700

    Time: 134 ms (84.89%), Space: 121.3 MB (39.01%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -29,23 +29,23 @@ class TimeMap {
         return result;
     }
 
-    private static class Pair<K, V>{
-        private final K key;
-        private final V value;
+    // private static class Pair<K, V>{
+    //     private final K key;
+    //     private final V value;
 
-        public Pair(K key, V value){
-            this.key = key;
-            this.value = value;
-        }
+    //     public Pair(K key, V value){
+    //         this.key = key;
+    //         this.value = value;
+    //     }
 
-        public K getKey(){
-            return key;
-        }
+    //     public K getKey(){
+    //         return key;
+    //     }
 
-        public V getValue(){
-            return value;
-        }
-    }
+    //     public V getValue(){
+    //         return value;
+    //     }
+    // }
 }
 
 /**
