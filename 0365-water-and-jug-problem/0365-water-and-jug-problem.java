547784bdf8e195556e87ecee0c4aac7a989da172
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-04-06 13:09:16 -0700

    Time: 0 ms (100%), Space: 40.2 MB (93.68%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.java
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -1,19 +1,27 @@
 class Solution {
     public boolean canMeasureWater(int x, int y, int target) {
-   // If target is greater than the combined capacity of both jugs return false
-        if (target > x + y) return false;
-   // If target is 0, we can always measure it by doing nothing
-        if (target == 0) return true;
-   // Calculate the GCD of x and y
-        int gcd = gcd(x, y);
-        return target % gcd == 0;
-    }
-     private static int gcd(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+        if(target > x + y){
+            return false;
         }
+
+        if(target == 0){
+            return true;
+        }
+
+        int hcf = gcd(x, y);
+        return target % hcf == 0;
+    }
+
+    private static int gcd(int a, int b) {
+        while(b != 0){
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+
         return a;
     }
 }
+
+//Time complexity: O(log(min(x, y)))
+//Space Complexity: O(1)
