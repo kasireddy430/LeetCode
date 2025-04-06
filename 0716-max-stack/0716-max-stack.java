dfa7953e923b5cfe72a91b44806554ace650d5d0
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-04-05 21:59:56 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0716-max-stack/0716-max-stack.java
+++ 0716-max-stack/0716-max-stack.java
@@ -1,51 +1,67 @@
 class MaxStack {
+
     private Stack<int[]> stack;
-    private Queue<int[]> heap;
+    private Queue<int[]> queue;
     private Set<Integer> removed;
-    private int cnt;
+    private int counter;
 
     public MaxStack() {
         stack = new Stack<>();
-        heap = new PriorityQueue<>((a, b) -> b[0] - a[0] == 0 ? b[1] - a[1] : b[0] - a[0]);
+        queue = new PriorityQueue<>((a, b) -> b[0] - a[0] == 0 ? b[1] - a[1] : b[0] - a[0]);
         removed = new HashSet<>();
+        counter = 0;
     }
 
     public void push(int x) {
-        stack.add(new int[] { x, cnt });
-        heap.add(new int[] { x, cnt });
-        cnt++;
+        stack.push(new int[]{x, counter});
+        queue.offer(new int[]{x, counter});
+        counter++;
     }
 
     public int pop() {
-        while (removed.contains(stack.peek()[1])) {
+        while(removed.contains(stack.peek()[1])){
             stack.pop();
         }
+
         int[] top = stack.pop();
         removed.add(top[1]);
         return top[0];
     }
 
     public int top() {
-        while (removed.contains(stack.peek()[1])) {
+        while(removed.contains(stack.peek()[1])){
             stack.pop();
         }
+
         return stack.peek()[0];
     }
 
     public int peekMax() {
-        while (removed.contains(heap.peek()[1])) {
-            heap.poll();
+        while(removed.contains(queue.peek()[1])){
+            queue.poll();
         }
-        return heap.peek()[0];
 
+        return max[0];       
     }
 
     public int popMax() {
-        while (removed.contains(heap.peek()[1])) {
-            heap.poll();
+        while(removed.contains(queue.peek()[1])){
+            queue.poll();
         }
-        int[] top = heap.poll();
-        removed.add(top[1]);
-        return top[0];
+
+        int[] max = queue.poll();
+        removed.add(max[1]);
+        return max[0];  
     }
 }
+
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
