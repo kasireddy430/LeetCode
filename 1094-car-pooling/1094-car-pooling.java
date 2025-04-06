60e5969b20922caca060e4dd29ce2ca4cdd34af1
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-04-05 19:14:49 -0700

    Time: 1 ms (100%), Space: 44.7 MB (25.49%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,17 +1,26 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
         int[] diff = new int[1001];
-        for (int[] trip : trips) {
-            diff[trip[1]] += trip[0];
-            diff[trip[2]] -= trip[0];
+
+        for(int[] trip : trips){
+            int passengersCount = trip[0];
+            int start = trip[1];
+            int end = trip[2];
+
+            diff[start] += passengersCount;
+            diff[end] -= passengersCount;
         }
+
         int sum = 0;
-        for (int i : diff) {
+
+        for(int i : diff){
             sum += i;
-            if (sum > capacity) {
-                return false;
-            }
+
+            if(sum > capacity) return false;
         }
-        return true;     
+        return true;
     }
 }
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
