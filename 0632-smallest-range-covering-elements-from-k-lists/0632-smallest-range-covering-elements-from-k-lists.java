20830a129c5d5aafa09029a1bb138fd945a63909
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-04-07 12:47:42 -0700

    Time: 97 ms (71.08%), Space: 70.3 MB (53.63%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,39 +1,36 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        // Min-Heap: stores (value, list index, element index)
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         int curMax = Integer.MIN_VALUE;
-
-        // Initialize the heap with the first element of each list
-        for (int i = 0; i < nums.size(); i++) {
-            minHeap.offer(new int[]{nums.get(i).get(0), i, 0});
-            curMax = Math.max(curMax, nums.get(i).get(0));
-        }
-
-        // Track the smallest range
         int[] smallRange = new int[]{0, Integer.MAX_VALUE};
 
-        while (true) {
-            // Get the minimum element from the heap
-            int[] curr = minHeap.poll();
-            int curMin = curr[0], listIdx = curr[1], elemIdx = curr[2];
+        for(int i = 0; i < nums.size(); i++){
+            int val = nums.get(i).get(0);
+            heap.offer(new int[]{val, i, 0});
+            curMax = Math.max(curMax, val);
+        }
 
-            // Update the smallest range if a better one is found
-            if (curMax - curMin < smallRange[1] - smallRange[0]) {
+        while(true){
+            int[] cur = heap.poll();
+            int curMin = cur[0], listIndex = cur[1], elemIndex = cur[2];
+
+            if(curMax - curMin < smallRange[1] - smallRange[0]){
                 smallRange[0] = curMin;
                 smallRange[1] = curMax;
             }
 
-            // Move to the next element in the same list
-            if (elemIdx + 1 < nums.get(listIdx).size()) {
-                int nextVal = nums.get(listIdx).get(elemIdx + 1);
-                minHeap.offer(new int[]{nextVal, listIdx, elemIdx + 1});
+            if(elemIndex + 1 < nums.get(listIndex).size()){
+                int nextVal = nums.get(listIndex).get(elemIndex + 1);
+                heap.offer(new int[]{nextVal, listIndex, elemIndex + 1});
                 curMax = Math.max(curMax, nextVal);
             } else {
-                // If any list is exhausted, stop
                 break;
             }
         }
+
         return smallRange;
     }
 }
+
+//Time Complexity: O(n log k)
+//Space Complexity: O(k)
