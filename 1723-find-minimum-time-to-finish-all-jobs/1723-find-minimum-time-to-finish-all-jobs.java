c6458e2ca3ade66491368f52694a2d06106f966c
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-04-06 19:42:51 -0700

    Time: 0 ms (100%), Space: 41.4 MB (38.4%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.java
@@ -1,35 +1,38 @@
 class Solution {
     public int minimumTimeRequired(int[] jobs, int k) {
-        // start will max from jobs
-        int start = 0;
-        // end is sum of all jobs
-        int end = 0;
-        Arrays.sort(jobs);
-        for(int job: jobs) {
+        int start = 0, end = 0;
+        Arrays.sort(jobs); // Sort jobs to assign larger ones first via reverse order
+        for (int job : jobs) {
             start = Math.max(start, job);
             end += job;
         }
-        int result = -1;
-        while(start < end) {
+
+        while (start < end) {
             int mid = start + (end - start) / 2;
-            if(find(jobs, new int[k], 0, mid)) {
-                result = mid; 
-                end = mid;
-            } else start = mid + 1;
+            if (canDistribute(jobs, new int[k], jobs.length - 1, mid)) {
+                end = mid; // Try smaller max load
+            } else {
+                start = mid + 1; // Increase allowed max load
+            }
         }
+
         return end;
     }
 
-    private boolean find(int[] jobs, int[] sum, int index, int mid) {
-        if(index == jobs.length) return true;
-        for(int i = 0; i < sum.length; i++) {
-            if(sum[i] + jobs[index] <= mid) {
-                sum[i] += jobs[index];
-                if(find(jobs, sum, index + 1, mid)) return true;
-                sum[i] -= jobs[index];
+    private boolean canDistribute(int[] jobs, int[] workloads, int index, int limit) {
+        if (index < 0) return true;
+
+        for (int i = 0; i < workloads.length; i++) {
+            if (workloads[i] + jobs[index] <= limit) {
+                workloads[i] += jobs[index];
+                if (canDistribute(jobs, workloads, index - 1, limit)) return true;
+                workloads[i] -= jobs[index];
             }
-            if(sum[i] == 0) break;
+
+            // Prune: if the worker has no job yet, skip trying other empty workers
+            if (workloads[i] == 0) break;
         }
+
         return false;
     }
 }
