0afabca2225c868ec138157c434498b4fc1af12a
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-05-27 15:14:02 -0700

    Time: 0 ms (100%), Space: 41 MB (76.96%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        
         int rob1 = 0, rob2 = 0;
 
         for(int num : nums){
