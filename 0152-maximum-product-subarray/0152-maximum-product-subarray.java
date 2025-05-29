5ed75be9a4c621f0a7bffa2c4c67798be85f9d28
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-05-29 11:53:58 -0700

    Time: 2 ms (56.15%), Space: 45.3 MB (14.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,14 +3,13 @@ class Solution {
         int maxProduct = nums[0], minProduct = nums[0], globalProduct = nums[0];
 
         for(int i = 1; i < nums.length; i++){
-            int current = nums[i];
+            int cur = nums[i];
 
-            int tempMax = Math.max(current, Math.max(maxProduct * current, minProduct * current));
-            minProduct = Math.min(current, Math.min(maxProduct * current, minProduct * current));
+            int tmpMax = Math.max(cur, Math.max(cur * maxProduct, cur * minProduct));
+            minProduct = Math.min(cur, Math.min(cur * maxProduct, cur * minProduct));
+            maxProduct = tmpMax;
 
-            maxProduct = tempMax;
             globalProduct = Math.max(globalProduct, maxProduct);
-
         }
 
         return globalProduct;
@@ -18,4 +17,4 @@ class Solution {
 }
 
 //Time Complexity: O(n)
-//Space COmplexity: O(1)
+//Space Complexity: O(1)
