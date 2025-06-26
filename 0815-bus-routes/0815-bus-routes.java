0b86ff534629e2f3a65449d707f2b9b9cb429e9c
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-06-26 15:19:05 -0700

    Time: 30 ms (98.12%), Space: 73.9 MB (62.51%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.java
+++ 0815-bus-routes/0815-bus-routes.java
@@ -1,74 +1,59 @@
 class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
-        /*Create a hashMap such that we can easily access inverted mappings(or we can say inverted relations) of that of the input. 
-		ie, in input we had:
-		
-        Bus Number(depicted as row in 2D input called routes[][]) -> Bus Stops(depicted as cols in 2D input called routes[][])
-        now using hashMap we attempt to establish:
-        
-		Bus Stop(Integer) -> Bus Number(ArrayList<Integer>), and this relation means that on a bus stop, which Buses can we expect
-        
-        All this is done because the cost of reaching all the bus stops for a given bus number is same irrespective intermediate nodes hopped */
-		
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap();
-        
-        for(int i = 0; i < routes.length; i++){
-            for(int j=0; j< routes[i].length; j++){
-                int busStopNo = routes[i][j];
-                ArrayList<Integer> busNo = map.getOrDefault(busStopNo, new ArrayList<>());
-                busNo.add(i);
-                map.put(busStopNo, busNo);
+        if (source == target) {
+            return 0;
+        }
+        int maxStation = 0;
+        Map<Integer, List<Integer>> stationToBus = new HashMap<>();
+
+        int buses = routes.length;
+
+        for (int i = 0; i < buses; i++) {
+            for (int station : routes[i]) {
+                stationToBus.computeIfAbsent(station, k -> new ArrayList<>()).add(i);
+                maxStation = Math.max(maxStation, station);
             }
         }
-        
-        
-        //we need a queue for BFS
-        LinkedList<Integer> queue = new LinkedList<>();
-		
-        //we need a hashSet to keep track of Bus Stops visited
-        HashSet<Integer> busStopVisited = new HashSet<>();
-		
-        //basically store the bus number to keep track of all bus stops visited for a Bus Number..kinda bundle up the work of busStopVisited(hashSet made just above) and reduce time complexity of rechecking all bus stops for a given Bus.
-        HashSet<Integer> busVisited = new HashSet<>();
-       
-        int cost = 0;
-        
-        //add source to the queue and make it visited
-        queue.addLast(source);
-        busStopVisited.add(source);
-		
-        //start BFS
-        while(queue.size()>0){
-            int size = queue.size();
-            while(size-->0){
-                int rem = queue.removeFirst();
-                // if the removed bus stop is equal to our target bus stop, then return the cost(cost is incermented when a different bus number is taken to reach destination)
-                if(rem == target){
-                    return cost;
+
+
+        if (!stationToBus.containsKey(source) || !stationToBus.containsKey(target)) {
+            return -1;
+        }
+        int busOffset = maxStation + 1;
+        int totalNodes = busOffset + buses;
+        //System.out.println(busOffset + " " + totalNodes);
+        int[] dist = new int[totalNodes];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        dist[source] = 0;
+        Deque<Integer> q = new ArrayDeque<>();
+
+        q.add(source);
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            //System.out.println(node);
+            if (node == target) {
+                return dist[target];
+            }
+            if (node < busOffset) {
+                for (int bus : stationToBus.get(node)) {
+                    int busNode = busOffset + bus;
+                    if (dist[busNode] > dist[node] + 1) {
+                        dist[busNode] = dist[node] + 1;
+                        q.addLast(busNode);
+                    }
                 }
-                //else
-                //use the hashmap to get the list of buses we can get form the bus stop on which we currently stand
-                ArrayList<Integer> buses = map.get(rem);
-                //think as if we are trying to catch and explore a new bus from the bus stop on which we are
-                for(int bus: buses){
-                    //already visited then don't do anything, just continue to explore new buses
-                    if(busVisited.contains(bus)==true){
-                        continue;
+            } else {
+                int bus = node - busOffset;
+
+                for (int station : routes[bus]) {
+                    if (dist[station] > dist[node]) {
+                        dist[station] = dist[node];
+                        q.addFirst(station);
                     }
-                    //new bus found->  now catch this bus and see where can you reach from this bus with a hope that this bus helps us get to our target bus stop when in the next BFS iteration we remove the busStops of this bus and test the condition if(rem == target) return cost;
-                    int[] arr = routes[bus];
-                    for(int busStop: arr){
-                        if(busStopVisited.contains(busStop)==true){
-                            continue;
-                        }
-                        queue.addLast(busStop);
-                        busStopVisited.add(busStop);
-                    }
-                    busVisited.add(bus);
                 }
             }
-            // we have taken a new bus so increase the cost
-            cost++;
         }
         return -1;
     }
