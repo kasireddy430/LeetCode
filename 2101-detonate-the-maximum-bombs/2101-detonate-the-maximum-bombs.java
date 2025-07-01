8df017a810ba86dfba53700edd0d07c73ade7e46
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-01 15:12:22 -0700

    Time: 12 ms (100%), Space: 44.9 MB (71.61%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,26 +1,38 @@
 class Solution {
- public int maximumDetonation(int[][] bombs) {
-        int n = bombs.length, ans = 0;
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
+        List<Integer>[] g = new ArrayList[n];
+        Arrays.setAll(g, i -> new ArrayList<>());
         for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, dfs(i, new boolean[n], bombs));
+            long x = bombs[i][0];
+            long y = bombs[i][1];
+            long r = bombs[i][2];
+            for (int j = 0; j < n; j++) {
+                long dx = x - bombs[j][0];
+                long dy = y - bombs[j][1];
+                if (j != i && dx * dx + dy * dy <= r * r) {
+                    g[i].add(j); // i 可以引爆 j
+                }
+            }
+        }
+
+        int ans = 0;
+        boolean[] vis = new boolean[n];
+        for (int i = 0; i < n && ans < n; i++) {
+            Arrays.fill(vis, false);
+            ans = Math.max(ans, dfs(g, vis, i));
         }
         return ans;
     }
 
-    private int dfs(int idx, boolean[] v, int[][] bombs) {
-        int count = 1;
-        v[idx] = true;
-        int n = bombs.length;
-        for (int i = 0; i < n; i++) {
-            if (!v[i] && inRange(bombs[idx], bombs[i])) {
-                count += dfs(i, v, bombs);
+    private int dfs(List<Integer>[] g, boolean[] vis, int x) {
+        vis[x] = true;
+        int cnt = 1;
+        for (int y : g[x]) {
+            if (!vis[y]) {
+                cnt += dfs(g, vis, y);
             }
         }
-        return count;
-    }
-
-    private boolean inRange(int[] a, int[] b) {
-        long dx = a[0] - b[0], dy = a[1] - b[1], r = a[2];
-        return dx * dx + dy * dy <= r * r;
+        return cnt;
     }
 }
