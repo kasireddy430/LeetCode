6a5a016b2ce4686db27fff406ec248141261631e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-01 15:41:42 -0700

    Time: 72 ms (92.13%), Space: 45.1 MB (46.12%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,43 +1,48 @@
 class Solution {
     public int maximumDetonation(int[][] bombs) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        int n = bombs.length;
-        
-        // Build the graph
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int xi = bombs[i][0], yi = bombs[i][1], ri = bombs[i][2];
-                int xj = bombs[j][0], yj = bombs[j][1];
-                
-                // Create a path from node i to node j, if bomb i detonates bomb j.
-                if ((long)ri * ri >= (long)(xi - xj) * (xi - xj) + (long)(yi - yj) * (yi - yj)) {
-                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
-                }
-            }
+        // Max number of bombs detonated
+        int max = 0;
+
+        // Detanote each bomb, and track number of bombs detonated
+        for (int i = 0; i < bombs.length; i++) {
+            max = Math.max(max, bfs(i, bombs));
         }
         
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, bfs(i, graph));
-        }
-        
-        return answer;
+        return max;
     }
-    
-    private int bfs(int i, Map<Integer, List<Integer>> graph) {
-        Deque<Integer> queue = new ArrayDeque<>();
-        Set<Integer> visited = new HashSet<>();
-        queue.offer(i);
-        visited.add(i);
+
+    private int bfs(int k, int[][] bombs) {
+        int count = 1; // number of detonated bombs;
+
+        int n = bombs.length; // number of bombs
+        
+        // Track the bombs that have been detonated
+        boolean[] visited = new boolean[n];
+        visited[k] = true;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(k);
+
         while (!queue.isEmpty()) {
-            int cur = queue.poll();
-            for (int neib : graph.getOrDefault(cur, new ArrayList<>())) {
-                if (!visited.contains(neib)) {
-                    visited.add(neib);
-                    queue.offer(neib);
+            int i = queue.poll();
+            for (int j = 0; j < n; j++) {
+                if (!visited[j] && isInRange(bombs[i], bombs[j])) {
+                    queue.offer(j);
+                    visited[j] = true;
+                    count++;
                 }
             }
         }
-        return visited.size();
+
+        return count;
+    }
+
+    // check if bomb2 is in range to be detonated by bomb1
+    public boolean isInRange(int[] bomb1, int[] bomb2) {
+        long dx = bomb1[0] - bomb2[0];
+        long dy = bomb1[1] - bomb2[1];
+        long r = bomb1[2];
+        
+        return dx*dx + dy*dy <= r*r; 
     }
 }
