8dc59c1acabdf1eee39b3905155f206caf77f7ee
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-05 15:24:30 -0700

    Time: 29 ms (31.81%), Space: 47.9 MB (45.1%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,40 +1,28 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        // Step1. Count numbers
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            count.put(nums[i], count.getOrDefault(nums[i], 0) + 1);
+        HashMap<Integer,Integer> first=new HashMap<>();
+        HashMap<Integer,Integer> last=new HashMap<>();
+        HashMap<Integer,Integer> count=new HashMap<>();
+
+        int degree=0;
+        int minL=nums.length;
+
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            first.putIfAbsent(num,i);
+            last.put(num,i);
+            count.put(num,count.getOrDefault(num,0)+1);
+
+            degree=Math.max(degree,count.get(num));
         }
-        
-        // Step2. Find the degree of the whole array, which is used in sliding windwo algorithm to determine window status
-        int degree = Collections.max(count.values());
-        
-        // Step3. Run sliding window. 
-        // use a slow pointer to indicate the end of the window, and a scanning pointer 'fast' indicating the front of the window
-        int slow = 0;
-        int minLength = nums.length;
-        Map<Integer, Integer> windowCount = new HashMap<>(); // window counter
-        int windowDegree = 0;  // the degree in the window
-        
-        for(int fast = 0; fast < nums.length; fast++) {
-            windowCount.put(nums[fast], windowCount.getOrDefault(nums[fast],0) + 1);
-            windowDegree = Math.max(windowDegree, windowCount.get(nums[fast]));
-            // when the window degree equals to the global degree, we need to minimize the window size, to find the min length
-            while(windowDegree == degree) {
-                // update our result
-                minLength = Math.min(minLength, fast - slow + 1);
-                
-                // pop out the number at 'slow' pointer, and increment slow
-                windowCount.put(nums[slow], windowCount.get(nums[slow]) - 1);
-                slow++;
-                
-                // only when the number popped up equals to the newly added number, we can safely reduce the windowDegree
-                // thus break the loop
-                if(nums[slow - 1] == nums[fast]) {
-                    windowDegree--;
-                }
+
+        for(int key : count.keySet()){
+            if(count.get(key)==degree){
+                int length=last.get(key)-first.get(key)+1;
+                minL=Math.min(minL,length);
             }
         }
-        return minLength;
+        return minL;
+        
     }
 }
