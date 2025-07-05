98e637872c6773517ec2b5fc67266eade9a71834
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-05 15:27:03 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,28 +1,24 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        HashMap<Integer,Integer> first=new HashMap<>();
-        HashMap<Integer,Integer> last=new HashMap<>();
-        HashMap<Integer,Integer> count=new HashMap<>();
+        Map<Integer, Integer> left = new HashMap(),
+            right = new HashMap(), count = new HashMap();
 
-        int degree=0;
-        int minL=nums.length;
-
-        for(int i=0;i<nums.length;i++){
-            int num=nums[i];
-            first.putIfAbsent(num,i);
-            last.put(num,i);
-            count.put(num,count.getOrDefault(num,0)+1);
-
-            degree=Math.max(degree,count.get(num));
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            if (left.get(x) == null) {
+                left.put(x, I);
+            }
+            right.put(x, i);
+            count.put(x, count.getOrDefault(x, 0) + 1);
         }
 
-        for(int key : count.keySet()){
-            if(count.get(key)==degree){
-                int length=last.get(key)-first.get(key)+1;
-                minL=Math.min(minL,length);
+        int ans = nums.length;
+        int degree = Collections.max(count.values());
+        for (int x: count.keySet()) {
+            if (count.get(x) == degree) {
+                ans = Math.min(ans, right.get(x) - left.get(x) + 1);
             }
         }
-        return minL;
-        
+        return ans;
     }
 }
