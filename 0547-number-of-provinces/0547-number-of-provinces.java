a6227454a0e7b6fe8b38cf7c37f84c1ee9f60b84
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-09 11:29:29 -0700

    Time: 4 ms (17.81%), Space: 47.6 MB (66.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,34 +1,36 @@
 class Solution {
-
-    public void bfs(int node, int[][] isConnected, boolean[] visit) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(node);
-        visit[node] = true;
-
-        while (!q.isEmpty()) {
-            node = q.poll();
-
-            for (int i = 0; i < isConnected.length; i++) {
-                if (isConnected[node][i] == 1 && !visit[i]) {
-                    q.offer(i);
-                    visit[i] = true;
-                }
-            }
-        }
-    }
-
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        int numberOfComponents = 0;
         boolean[] visit = new boolean[n];
+        int components = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (!visit[i]) {
-                numberOfComponents++;
+        for(int i = 0; i < n; i++){
+            if(!visit[i]){
+                components++;
                 bfs(i, isConnected, visit);
             }
         }
 
-        return numberOfComponents;
+        return components;
+    }
+
+    private void bfs(int node, int[][] isConnected, boolean[] visit){
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(node);
+
+        while(!q.isEmpty()){
+            int cur = q.poll();
+            visit[cur] = true;
+
+            for(int i = 0; i < isConnected.length; i++){
+                if(isConnected[cur][i] == 1 && !visit[i]){
+                    q.offer(i);
+                }
+            }
+        }
     }
 }
+
+//TC: O(n)
+//SC: O(n)
