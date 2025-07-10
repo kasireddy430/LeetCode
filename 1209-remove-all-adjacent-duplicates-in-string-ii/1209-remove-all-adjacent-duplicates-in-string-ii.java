5a369d307f2249ae166b9e94471a44316657d949
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-10 15:33:15 -0700

    Time: 20 ms (78.69%), Space: 45.9 MB (31.24%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,21 +1,27 @@
 class Solution {
-public String removeDuplicates(String s, int k) {
-    Stack<Integer> counts = new Stack<>();
-    char[] sa = s.toCharArray();
-    int j = 0;
-    for (int i = 0; i < s.length(); ++i, ++j) {
-        sa[j] = sa[i];
-        if (j == 0 || sa[j] != sa[j - 1]) {
-            counts.push(1);
-        } else {
-            int incremented = counts.pop() + 1;
-            if (incremented == k) {
-                j = j - k;
-            } else {
-                counts.push(incremented);
+    public String removeDuplicates(String s, int k) {
+        Stack<Integer> count = new Stack<>();
+        char[] sa = s.toCharArray();
+        int j = 0;
+
+        for(int i = 0; i < s.length(); i++, j++){
+            sa[j] = sa[i];
+            if(j == 0 || sa[j] != sa[j - 1]){
+                count.push(1);
+            } else{
+                int incremented = count.pop() + 1;
+
+                if(incremented == k){
+                    j = j - k;
+                } else {
+                    count.push(incremented);
+                }
             }
         }
+
+        return new String(sa, 0 , j);
     }
-    return new String(sa, 0, j);
 }
-}
+
+//TC: O(n)
+//SC: O(n)
