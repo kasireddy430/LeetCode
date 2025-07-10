883bf6e7cacd91b73456a68a0072fffac7569e41
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-09 21:37:18 -0700

    Time: 101 ms (99.81%), Space: 86.2 MB (44.53%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.java
+++ 1429-first-unique-number/1429-first-unique-number.java
@@ -1,27 +1,65 @@
+class Node{
+    int val;
+    boolean isUnique;
+    Node prev;
+    Node next;
+
+    Node(int val){
+        this.val = val;
+        isUnique = true;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
 class FirstUnique {
-    
-    Map<Integer, Integer> map;
-    Queue<Integer> queue;
+    Map<Integer, Node> map;
+    Node head;
+    Node tail;
 
     public FirstUnique(int[] nums) {
         map = new HashMap<>();
-        queue = new LinkedList<>();
+        head = new Node(0);
+        tail = new Node(0);
+
+        head.next = tail;
+        tail.prev = head;
+
         for(int num : nums){
             add(num);
         }
     }
     
     public int showFirstUnique() {
-        while(!queue.isEmpty() && map.containsKey(queue.peek()) && map.get(queue.peek()) > 1)
-            queue.remove();
-        return queue.isEmpty() ? -1 : queue.peek();
+        if(head.next != tail){
+            return head.next.val;
+        }
+        return -1;
+    }
+
+    private void addList(Node node){
+        node.prev = tail.prev;
+        tail.prev.next = node;
+        node.next = tail;
+        tail.prev = node;
+    }
+
+    private void removeList(Node node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
     }
     
     public void add(int value) {
         if(!map.containsKey(value)){
-            map.put(value, 1);
-            queue.add(value);
-        }else
-            map.put(value, map.get(value) + 1);
+            Node node = new Node(value);
+            addList(node);
+            map.put(value, node);
+        } else {
+            Node cur = map.get(value);
+            if(cur.isUnique){
+                removeList(cur);
+            }
+            cur.isUnique = false;
+        }
     }
 }
