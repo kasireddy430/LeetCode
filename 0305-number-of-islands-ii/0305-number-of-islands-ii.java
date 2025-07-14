c964ac3fd5b0b3c8ca5c698e5b3e162a98b8f872
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-14 14:37:16 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0305-number-of-islands-ii/0305-number-of-islands-ii.java
+++ 0305-number-of-islands-ii/0305-number-of-islands-ii.java
@@ -1,81 +1,41 @@
-class UnionFind {
-    int[] parent;
-    int[] rank;
-    int count;
+public class Solution {
+    int[][] dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
 
-    public UnionFind(int size) {
-        parent = new int[size];
-        rank = new int[size];
-        for (int i = 0; i < size; i++)
-            parent[i] = -1;
-        count = 0;
-    }
-
-    public void addLand(int x) {
-        if (parent[x] >= 0)
-            return;
-        parent[x] = x;
-        count++;
-    }
-
-    public boolean isLand(int x) {
-        if (parent[x] >= 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    int numberOfIslands() {
-        return count;
-    }
-
-    public int find(int x) {
-        if (parent[x] != x)
-            parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    public void union(int x, int y) {
-        int xset = find(x), yset = find(y);
-        if (xset == yset) {
-            return;
-        } else if (rank[xset] < rank[yset]) {
-            parent[xset] = yset;
-        } else if (rank[xset] > rank[yset]) {
-            parent[yset] = xset;
-        } else {
-            parent[yset] = xset;
-            rank[xset]++;
-        }
-        count--;
-    }
-}
-
-class Solution {
     public List<Integer> numIslands2(int m, int n, int[][] positions) {
-        int x[] = { -1, 1, 0, 0 };
-        int y[] = { 0, 0, -1, 1 };
-        UnionFind dsu = new UnionFind(m * n);
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        if(m <= 0 || n <= 0) return result;
 
-        for (int[] position : positions) {
-            int landPosition = position[0] * n + position[1];
-            dsu.addLand(landPosition);
+        int count = 0;                      // number of islands
+        int[] roots = new int[m * n];       // one island = one tree
+        Arrays.fill(roots, -1);            
 
-            for (int i = 0; i < 4; i++) {
-                int neighborX = position[0] + x[i];
-                int neighborY = position[1] + y[i];
-                int neighborPosition = neighborX * n + neighborY;
-                // If neighborX and neighborY correspond to a point in the grid and there is a
-                // land at that point, then merge it with the current land.
-                if (neighborX >= 0 && neighborX < m && neighborY >= 0 && neighborY < n &&
-                        dsu.isLand(neighborPosition)) {
-                    dsu.union(landPosition, neighborPosition);
+        for(int[] p : positions) {
+            int root = n * p[0] + p[1];     // assume new point is isolated island
+            roots[root] = root;             // add new island
+            count++;
+
+            for(int[] dir : dirs) {
+                int x = p[0] + dir[0]; 
+                int y = p[1] + dir[1];
+                int nb = n * x + y;
+                if(x < 0 || x >= m || y < 0 || y >= n || roots[nb] == -1) continue;
+                
+                int rootNb = findIsland(roots, nb);
+                if(root != rootNb) {        // if neighbor is in another island
+                    roots[root] = rootNb;   // union two islands 
+                    root = rootNb;          // current tree root = joined tree root
+                    count--;               
                 }
             }
-            answer.add(dsu.numberOfIslands());
+
+            result.add(count);
         }
-        return answer;
+        return result;
     }
+
+    public int findIsland(int[] roots, int id) {
+        while(id != roots[id]) id = roots[id];
+        return id;
+    }
+
 }
