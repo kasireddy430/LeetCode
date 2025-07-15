1f9fc557ca12351675a67941f608c2fac871676c
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-14 21:15:39 -0700

    Time: 1321 ms (28.24%), Space: 148.6 MB (34%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.java
+++ 3161-block-placement-queries/3161-block-placement-queries.java
@@ -1,46 +1,48 @@
 class Solution {
     public List<Boolean> getResults(int[][] queries) {
-        TreeMap<Integer, Integer> obstacles = new TreeMap<>(); // max up until block at key
+        TreeMap<Integer, Integer> obstacles = new TreeMap<>();
 
         List<Boolean> ans = new ArrayList<>();
 
-        for (int[] q : queries) {
-            if (q[0] == 1) { // build obstacle
+        for(int[] q : queries){
+            if(q[0] == 1){
                 int index = q[1];
+
                 Integer prev = obstacles.lowerKey(index);
                 Integer next = obstacles.higherKey(index);
 
-                if (prev == null) {
+                if(prev == null){
                     obstacles.put(index, index);
                 } else {
                     obstacles.put(index, Math.max(index - prev, obstacles.get(prev)));
                 }
 
                 prev = index;
-                while (next != null && obstacles.get(next) > obstacles.get(prev)) {
+
+                while(next != null && obstacles.get(next) > obstacles.get(prev)){
                     obstacles.put(next, Math.max(next - prev, obstacles.get(prev)));
                     prev = next;
                     next = obstacles.higherKey(next);
                 }
-
-            } else { // ans query
+            } else {
                 int index = q[1], sz = q[2];
-                if (obstacles.containsKey(index)) { // there's an obstacle at queried index
+
+                if(obstacles.containsKey(index)){
                     ans.add(obstacles.get(index) >= sz);
-                } else { // no obstacle
+                } else {
+
                     Integer prev = obstacles.lowerKey(index);
-                    if (prev == null) {
+
+                    if(prev == null){
                         ans.add(index >= sz);
                     } else {
                         ans.add(Math.max(index - prev, obstacles.get(prev)) >= sz);
                     }
                 }
-
             }
         }
 
         return ans;
-
     }
 
 }
