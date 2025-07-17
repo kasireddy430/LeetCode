d55db830b5dd6929ac804bb038fda517bbcc7a55
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-17 16:32:32 -0700

    Time: 29 ms (85.61%), Space: 128.3 MB (53.79%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.java
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.java
@@ -1,13 +1,27 @@
 class Solution {
     public int minSideJumps(int[] obstacles) {
-         int[] dp = new int[]{1, 0, 2};
-        for (int a: obstacles) {
-            if (a > 0)
-                dp[a - 1] = 1000000;
-            for (int i = 0; i < 3; ++i)
-                if (a != i + 1)
-                    dp[i] = Math.min(dp[i], Math.min(dp[(i + 1) % 3], dp[(i + 2) % 3]) + 1);
+        int n = obstacles.length;
+        int[] dp = new int[3];
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for (int i = 1; i < n; i++) {
+           
+            for (int j = 0; j < 3; j++) {
+                if (obstacles[i] == j + 1) {
+                    dp[j] = Integer.MAX_VALUE;
+                }
+            }
+            
+            for (int j = 0; j < 3; j++) {
+                if (obstacles[i] != j + 1) {
+                    dp[j] = Math.min(dp[j], Math.min((dp[(j + 1) % 3] == Integer.MAX_VALUE ? Integer.MAX_VALUE : dp[(j + 1) % 3] + 1),
+                                                      (dp[(j + 2) % 3] == Integer.MAX_VALUE ? Integer.MAX_VALUE : dp[(j + 2) % 3] + 1)));
+                }
+            }
         }
+        
         return Math.min(dp[0], Math.min(dp[1], dp[2]));
     }
 }
