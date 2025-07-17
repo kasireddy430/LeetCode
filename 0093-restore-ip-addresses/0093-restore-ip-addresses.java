002f5d5462fe867b37794ae109dd83ee38213ef0
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-17 15:05:50 -0700

    Time: 5 ms (46.16%), Space: 42.7 MB (52.99%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,23 +1,30 @@
 class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
-        if (s.length() > 12) return res;
+        if (s.length() > 12 || s.length() < 4) return res;
 
         backtrack(s, 0, 0, "", res);
         return res;
     }
-    
-    public void backtrack(String s, int i, int dots, String currIp, List<String> res) {
+
+    private void backtrack(String s, int i, int dots, String currIp, List<String> res) {
         if (dots == 4 && i == s.length()) {
-            res.add(currIp.substring(0, currIp.length() - 1));
+            res.add(currIp.substring(0, currIp.length() - 1)); // Remove trailing dot
             return;
         }
-        if (dots > 4) return;
+        if (dots >= 4) return;
 
-        for (int j = i; j < Math.min(i+3, s.length()); j++) {
-            if (Integer.parseInt(s.substring(i, j+1)) < 256 && (i == j || s.charAt(i) != '0')) {
-                backtrack(s, j+1, dots+1, currIp + s.substring(i, j+1) + ".", res);
+        for (int j = i; j < i + 3 && j < s.length(); j++) {
+            String segment = s.substring(i, j + 1);
+            if (isValid(segment)) {
+                backtrack(s, j + 1, dots + 1, currIp + segment + ".", res);
             }
         }
+    }
+
+    private boolean isValid(String segment) {
+        if (segment.length() > 1 && segment.startsWith("0")) return false;
+        int val = Integer.parseInt(segment);
+        return val >= 0 && val <= 255;
     }
 }
