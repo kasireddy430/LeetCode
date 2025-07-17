b3f3c5229096fc75428485ffba8c0d07f4a54f91
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-17 15:30:07 -0700

    Time: 22 ms (97.35%), Space: 128.3 MB (51.14%) - LeetHub

--- 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.java
+++ 1824-minimum-sideway-jumps/1824-minimum-sideway-jumps.java
@@ -1,38 +1,13 @@
 class Solution {
     public int minSideJumps(int[] obstacles) {
-        int[] dp = new int[]{1, 0, 1};
-        for(int i=1; i<obstacles.length; i++){
-            switch(obstacles[i]){
-                case 0:
-                    dp[0] = min(dp[0], dp[1]+1, dp[2]+1);
-                    dp[1] = min(dp[0]+1, dp[1], dp[2]+1);
-                    dp[2] = min(dp[0]+1, dp[1]+1, dp[2]);
-                    break;
-                case 1:
-                    dp[0] = Integer.MAX_VALUE;
-                    dp[1] = min(dp[1], dp[2]+1);
-                    dp[2] = min(dp[1]+1, dp[2]);
-                    break;
-                case 2:
-                    dp[0] = min(dp[0], dp[2]+1);
-                    dp[1] = Integer.MAX_VALUE;
-                    dp[2] = min(dp[0]+1, dp[2]);
-                    break;
-                case 3:
-                    dp[0] = min(dp[0], dp[1]+1);
-                    dp[1] = min(dp[0]+1, dp[1]);
-                    dp[2] = Integer.MAX_VALUE;
-                    break;
-            }
+         int[] dp = new int[]{1, 0, 1};
+        for (int a: obstacles) {
+            if (a > 0)
+                dp[a - 1] = 1000000;
+            for (int i = 0; i < 3; ++i)
+                if (a != i + 1)
+                    dp[i] = Math.min(dp[i], Math.min(dp[(i + 1) % 3], dp[(i + 2) % 3]) + 1);
         }
-        return min(dp[0], dp[1], dp[2]);
-    }
-    
-    int min(int... vals){
-        int min = Integer.MAX_VALUE;
-        for(int val: vals){
-            if(val>=0) min = Math.min(min, val);
-        }
-        return min;
+        return Math.min(dp[0], Math.min(dp[1], dp[2]));
     }
 }
