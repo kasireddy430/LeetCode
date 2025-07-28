dcb5e89ef8937a6793b433f63fbbb21ba7f2ed8f
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-28 15:08:24 -0700

    Time: 7 ms (72.12%), Space: 40.9 MB (99.17%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,19 +1,10 @@
 public class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount + 1];
+        int[] dp = new int[amount + 1]; 
         dp[0] = 1;
-        for (int i = coins.length - 1; i >= 0; i--) {
-            int[] nextDP = new int[amount + 1];
-            nextDP[0] = 1;
-
-            for (int a = 1; a <= amount; a++) {
-                nextDP[a] = dp[a];
-                if (a - coins[i] >= 0) {
-                    nextDP[a] += nextDP[a - coins[i]];
-                }
-            }
-            dp = nextDP;
-        }
-        return dp[amount];
+        for (int i = coins.length - 1; i >= 0; i--)
+            for (int a = 1; a <= amount; a++) 
+                dp[a] = dp[a] + (coins[i] <= a ? dp[a - coins[i]] : 0);
+        return dp[amount]; 
     }
 }
