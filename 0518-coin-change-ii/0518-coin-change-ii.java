b00125f761dbd224835c965a0c9a1b0b81e0cdf8
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-07-28 15:08:06 -0700

    Time: 11 ms (33.48%), Space: 45.1 MB (66.13%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,19 @@
 public class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        Arrays.sort(coins);
-        int[][] dp = new int[n + 1][amount + 1];
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int i = coins.length - 1; i >= 0; i--) {
+            int[] nextDP = new int[amount + 1];
+            nextDP[0] = 1;
 
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int a = 0; a <= amount; a++) {
-                if (a >= coins[i]) {
-                    dp[i][a] = dp[i + 1][a];
-                    dp[i][a] += dp[i][a - coins[i]];
+            for (int a = 1; a <= amount; a++) {
+                nextDP[a] = dp[a];
+                if (a - coins[i] >= 0) {
+                    nextDP[a] += nextDP[a - coins[i]];
                 }
             }
+            dp = nextDP;
         }
-
-        return dp[0][amount];
+        return dp[amount];
     }
 }
