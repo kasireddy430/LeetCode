b29f8836189cac41b3adfb98c5ddd11f30199f92
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-05 14:15:25 -0700

    Time: 23 ms (91.37%), Space: 45.5 MB (74.49%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,35 +1,25 @@
-class Solution {
+public class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        //Base case: If hand.length is not multiple of groupSize, then we cannot form equal groups
-        if(hand.length % groupSize != 0) return false;
+        if (hand.length % groupSize != 0) return false;
 
-        //Create TreeMap to count frequencies of each card and it will be in sorted order
-        TreeMap<Integer, Integer> count = new TreeMap<>();
-        for(int num : hand){
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : hand) {
             count.put(num, count.getOrDefault(num, 0) + 1);
         }
 
-        //Process groups starting from the smallest available number
-        while(!count.isEmpty()){
-            //get the smallest card from treemap
-            int small = count.firstKey();
-
-            //Try to form groups of groupSize
-            for(int i = small; i < small + groupSize; i++){
-                //Return false if there is number missing, which indicates equal groups cannot be formed
-                if(!count.containsKey(i)){
-                    return false;
+        for (int num : hand) {
+            int start = num;
+            while (count.getOrDefault(start - 1, 0) > 0) start--;
+            while (start <= num) {
+                while (count.getOrDefault(start, 0) > 0) {
+                    for (int i = start; i < start + groupSize; i++) {
+                        if (count.getOrDefault(i, 0) == 0) return false;
+                        count.put(i, count.get(i) - 1);
+                    }
                 }
-
-                //Decrement the count frequency, since the card is used to form group
-                count.put(i, count.get(i) - 1);
-                //Remove card from TreeMap if count reaches 0
-                if(count.get(i)==0){
-                    count.remove(i);
-                }
+                start++;
             }
         }
-        //Return true if exits the loop, indicating equal groups
         return true;
     }
 }
