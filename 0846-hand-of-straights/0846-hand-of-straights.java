9718b33754de3c89951154b830512106efc1ecef
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-05 14:16:48 -0700

    Time: 61 ms (36.73%), Space: 45.3 MB (88.7%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -2,27 +2,23 @@ public class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
         if (hand.length % groupSize != 0) return false;
 
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : hand) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
+        TreeMap<Integer, Integer> count = new TreeMap<>();
+        for (int card : hand) {
+            count.put(card, count.getOrDefault(card, 0) + 1);
         }
 
-        for (int num : hand) {
-            int start = num;
-            while (count.getOrDefault(start - 1, 0) > 0) start--;
-            while (start <= num) {
-                while (count.getOrDefault(start, 0) > 0) {
-                    for (int i = start; i < start + groupSize; i++) {
-                        if (count.getOrDefault(i, 0) == 0) return false;
-                        count.put(i, count.get(i) - 1);
-                    }
+        while (!count.isEmpty()) {
+            int start = count.firstKey(); // always take the smallest number
+
+            for (int i = start; i < start + groupSize; i++) {
+                if (!count.containsKey(i)) return false;
+                count.put(i, count.get(i) - 1);
+                if (count.get(i) == 0) {
+                    count.remove(i);
                 }
-                start++;
             }
         }
+
         return true;
     }
 }
-
-//Time Complexity: O(n log n). Contributed by insertions into treeMap
-//Space Complexity: O(n)
