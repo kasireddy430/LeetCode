8956e0ec6fc73b918a3cb12b0ea9a75d800f2c92
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-05 23:53:40 -0700

    Time: 3 ms (7.01%), Space: 45.7 MB (45.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,59 +1,33 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //Calculate the row and column length of input matrix
-        int ROWS = matrix.length;
-        int COLS = matrix[0].length;
+        Queue<int[]> q = new LinkedList<>();
 
-        //Create a boolean variable for tackling the first row
-        boolean firstRow = false;
-
-        //Mark first row and first column of the input matrix
-        for(int r = 0; r < ROWS; r++){
-            for(int c = 0; c < COLS; c++){
-                //Check if current cell is 0
-                if(matrix[r][c] == 0){
-                    //If so, Mark first row and the first column with 0 respective to current cell
-                    matrix[0][c] = 0;
-                    //If current cell is not part of first row
-                    if(r > 0){
-                        //Mark first row respective to current cell
-                        matrix[r][0] = 0;
-                    } else{
-                        //If first row needs to be zeroed out update boolean value
-                        firstRow = true;
-                    }
+        for(int i = 0 ; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){  
+                if(matrix[i][j] == 0){
+                    q.offer(new int[]{i, j});
                 }
             }
         }
 
-        //Mark all the rows and columns except first row 
-        //and first column with zero
-        for(int r = 1; r < ROWS; r++){
-            for(int c = 1; c < COLS; c++){
-                //If any of the first row or column is marked then make current cell 0
-                if(matrix[0][c] == 0 || matrix[r][0] == 0){
-                    matrix[r][c] = 0;
-                }
-            }
-        }
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        //Mark first column zero if necessary
-        if(matrix[0][0] == 0){
-            for(int r = 1; r < ROWS; r++){
-                //Mark entire colum zero
-                matrix[r][0] = 0;
-            }
-        }
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int row = cur[0];
+            int col = cur[1];
 
-        //Mark first row zero if required
-        if(firstRow){
-            for(int c = 0; c < COLS; c++){
-                //Mark first row 0
-                matrix[0][c] = 0;
+            for(int i = 0; i < matrix.length; i++){
+                matrix[i][col] = 0;
+            }
+
+            for(int i = 0; i < matrix[0].length; i++){
+                matrix[row][i] = 0;
             }
         }
     }
 }
+
 
 //Time Complexity: O(m * n)
 //Space Complexity: O(1)
