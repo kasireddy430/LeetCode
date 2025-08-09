4ec5e5c7312e8c4ca281d8b38cd2841ed780016d
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-09 13:59:56 -0700

    Time: 8 ms (85.67%), Space: 45.4 MB (48.98%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -1,53 +1,66 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class Solution {
-  private int n;
-  private int[][] moves = new int[][] {{-1,0}, {1,0}, {0,-1}, {0,1}};
-  private Queue<int[]> queue = new ArrayDeque<>();
+    private static final int ISLAND = 1;
+    private static final int VISITED_ISLAND = 2;
+    private static final int WATER = 0;
+    private static final int VISITED_WATER = -1;
 
-  private void discoverIsland(int[][] grid, int x, int y) {
-    grid[x][y] = 2;
-    queue.add(new int[] {x, y});
+    private int n;
+    private final int[][] moves = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-    for (var move : moves) {
-      var x2 = x + move[0];
-      var y2 = y + move[1];
+    private void discoverIsland(int[][] grid, int row, int col, Queue<int[]> queue) {
+        grid[row][col] = VISITED_ISLAND;
+        queue.offer(new int[] {row, col});
 
-      if (x2 >= 0 && x2 < n && y2 >= 0 && y2 < n)
-        if (grid[x2][y2] == 1)
-          discoverIsland(grid, x2, y2);
-    }
-  }
+        for (int[] move : moves) {
+            int newRow = row + move[0];
+            int newCol = col + move[1];
 
-  public int shortestBridge(int[][] grid) {
-    n = grid.length;
-    int x = 0, y = 0;
-    var br = false;
-
-    for (x=0; x<n && !br; x++) {
-      for (y=0; y<n && !br; y++) {
-        if (grid[x][y] == 1) {
-          discoverIsland(grid, x, y);
-          br = true;
-        }
-      }
-    }
-    for (var cnt = 0; ; cnt++) {
-      for (var i = queue.size(); i > 0; i--) {
-        var pos = queue.poll();
-
-        for (var move : moves) {
-          var x2 = pos[0] + move[0];
-          var y2 = pos[1] + move[1];
-
-          if (x2 >= 0 && x2 < n && y2 >= 0 && y2 < n) {
-            if (grid[x2][y2] == 1)
-              return cnt;
-            else if (grid[x2][y2] == 0) {
-              queue.add(new int[] {x2, y2});
-              grid[x2][y2] = -1;
+            if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n &&
+                grid[newRow][newCol] == ISLAND) {
+                discoverIsland(grid, newRow, newCol, queue);
             }
-          }
         }
-      }
     }
-  }
+
+    public int shortestBridge(int[][] grid) {
+        n = grid.length;
+        Queue<int[]> queue = new ArrayDeque<>();
+
+        // 1. Find and mark the first island
+        boolean found = false;
+        for (int r = 0; r < n && !found; r++) {
+            for (int c = 0; c < n && !found; c++) {
+                if (grid[r][c] == ISLAND) {
+                    discoverIsland(grid, r, c, queue);
+                    found = true;
+                }
+            }
+        }
+
+        // 2. BFS expansion
+        int steps = 0;
+        while (true) {
+            for (int size = queue.size(); size > 0; size--) {
+                int[] pos = queue.poll();
+                for (int[] move : moves) {
+                    int newRow = pos[0] + move[0];
+                    int newCol = pos[1] + move[1];
+
+                    if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < n) {
+                        if (grid[newRow][newCol] == ISLAND) {
+                            return steps;
+                        }
+                        if (grid[newRow][newCol] == WATER) {
+                            grid[newRow][newCol] = VISITED_WATER;
+                            queue.offer(new int[]{newRow, newCol});
+                        }
+                    }
+                }
+            }
+            steps++;
+        }
+    }
 }
