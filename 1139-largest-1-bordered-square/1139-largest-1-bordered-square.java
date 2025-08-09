accf5185c46f7dc835273e4f2aaf2bd4f936543b
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-09 15:50:53 -0700

    Time: 8 ms (38.49%), Space: 45.7 MB (45.61%) - LeetHub

--- 1139-largest-1-bordered-square/1139-largest-1-bordered-square.java
+++ 1139-largest-1-bordered-square/1139-largest-1-bordered-square.java
@@ -1,26 +1,29 @@
 class Solution {
     public int largest1BorderedSquare(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] down = new int[m][n];
-        int[][] right = new int[m][n];
-        for (int i = m - 1; i >= 0; --i) {
-            for (int j = n - 1; j >= 0; --j) {
-                if (grid[i][j] == 1) {
-                    down[i][j] = i + 1 < m ? down[i + 1][j] + 1 : 1;
-                    right[i][j] = j + 1 < n ? right[i][j + 1] + 1 : 1;
+        int[][] left = new int[m][n], top = new int[m][n];
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    top[i][j] = grid[i][j];
+                } else {
+                    top[i][j] = grid[i][j] == 0? 0 : top[i-1][j] + 1;
                 }
-            }
-        }
-        for (int k = Math.min(m, n); k > 0; --k) {
-            for (int i = 0; i <= m - k; ++i) {
-                for (int j = 0; j <= n - k; ++j) {
-                    if (down[i][j] >= k && right[i][j] >= k && right[i + k - 1][j] >= k
-                        && down[i][j + k - 1] >= k) {
-                        return k * k;
+                if (j == 0) {
+                    left[i][j] = grid[i][j];
+                } else {
+                    left[i][j] = grid[i][j] == 0? 0 : left[i][j-1] + 1;
+                }
+                for (int l = Math.min(top[i][j], left[i][j]); l >= 1; l--) {
+				// 已知下面的边和右侧边合法，看上边和左侧边是否合法
+                    if (left[i-l+1][j] >= l && top[i][j-l+1] >= l) {
+                        res = Math.max(res, l * l);
+                        break;
                     }
                 }
             }
         }
-        return 0;
+        return res;
     }
 }
