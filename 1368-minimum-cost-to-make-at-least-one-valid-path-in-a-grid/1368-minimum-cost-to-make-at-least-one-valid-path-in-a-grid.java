3ae3bfb98e2dffb2ab583ea14d041b27d76cc6aa
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-10 14:54:41 -0700

    Time: 13 ms (76.24%), Space: 45.5 MB (26.45%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,44 +1,42 @@
 class Solution {
     public int minCost(int[][] grid) {
-        int n = grid[0].length;
-        int m = grid.length;
+        int m=grid.length;
+        int n=grid[0].length;
 
-        int[] dr = {0,  0,  1, -1};
-        int[] dc = {1, -1,  0,  0};
-
-        int[][] dist = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-                dist[i][j] = Integer.MAX_VALUE;
+        int[][] minCost=new int[m][n];
+        for(int i=0; i<m; i++){
+            Arrays.fill(minCost[i], Integer.MAX_VALUE);
         }
-        dist[0][0] = 0;
-        Deque<int[]> deq = new ArrayDeque<>();
-        deq.addFirst(new int[]{0,0});
 
-        while(!deq.isEmpty()){
-            int[] curr = deq.removeFirst();
-            int r = curr[0];
-            int c = curr[1];
-            if (r == m-1 && c == n-1) 
-                return dist[r][c];
-            for(int i=1;i<=4;i++){
-                int k = i-1;
-                int nr = r+dr[k];
-                int nc = c+dc[k];
-                if(nr < m && nc < n && nr >= 0 && nc >= 0){
-                    int extra = i == grid[r][c] ? 0 : 1;
-                    if(dist[r][c]+extra<dist[nr][nc])
-                    {
-                        dist[nr][nc] = dist[r][c]+extra;
-                        if (extra == 0)
-                            deq.addFirst(new int[]{nr,nc});
-                        else
-                            deq.addLast(new int[]{nr,nc});
-                    }
-                        
+        Deque<int[]> dque=new ArrayDeque<>();
+        dque.offerFirst(new int[]{0,0});
+        minCost[0][0]=0;
+        
+        int[][] direction={ {0,1}, {0,-1}, {1,0}, {-1,0} };
+
+        while(!dque.isEmpty()){
+            int[] arr=dque.pollFirst();
+            int r=arr[0];
+            int c=arr[1];
+
+            //Visited adjacent cells
+            for(int i=0; i<4; i++){
+                int nr=r+direction[i][0];
+                int nc=c+direction[i][1];
+                int cost=(grid[r][c]!=(i+1))?1:0;
+
+                if( nr>=0 && nr<m && nc>=0 && nc<n && minCost[r][c]+cost<minCost[nr][nc]){
+                    minCost[nr][nc]=minCost[r][c]+cost;
+
+                    if(cost==1){
+                        dque.offerLast(new int[]{nr,nc});
+                    }else{
+                        dque.offerFirst(new int[]{nr,nc});
+                    }                   
                 }
             }
         }
-        return dist[m-1][n-1];
+
+        return minCost[m-1][n-1];
     }
 }
