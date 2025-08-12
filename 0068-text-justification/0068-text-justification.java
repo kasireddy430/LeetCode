b48b104a16d6eb701b4371e346de3dd1f16f8d20
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-12 15:39:11 -0700

    Time: 0 ms (100%), Space: 41.7 MB (72.5%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,41 +1,49 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> result = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         int i = 0;
-        while (i < words.length) {
+
+        while(i < words.length){
             int j = i, len = 0;
-            while (j < words.length && len + words[j].length() + (j - i) <= maxWidth) {
+
+            while(j < words.length && len + words[j].length() + (j - i) <= maxWidth){
                 len += words[j].length();
                 j++;
             }
-            
-            int gaps = j - i - 1;
-            int spaces = maxWidth - len;
+
+            int gaps = j - (i + 1);
+            int space = maxWidth - len;
             StringBuilder line = new StringBuilder();
 
-            if (j == words.length || gaps == 0) {
-                for (int k = i; k < j; k++) {
+            if(j == words.length || gaps == 0){
+                for(int k = i; k < j; k++){
                     line.append(words[k]);
-                    if (k != j - 1) line.append(" ");
+                    if(k != j - 1) line.append(" ");
                 }
-                while (line.length() < maxWidth) line.append(" ");
+
+                while(line.length() < maxWidth) line.append(" ");
             } else {
-                int spaceEach = spaces / gaps, extra = spaces % gaps;
-                for (int k = i; k < j; k++) {
+                int spaceEach = space/gaps;
+                int extra = space % gaps;
+
+                for(int k = i; k < j; k++){
+
                     line.append(words[k]);
-                    if (k != j - 1) { // if not last word
+                    if(k != j - 1){
                         int toAdd = spaceEach;
-                        if (extra > 0) {   // still have extra spaces to distribute
+                        if(extra > 0){
                             toAdd += 1;
                             extra--;
                         }
                         line.append(" ".repeat(toAdd));
-                    }
+                    } 
                 }
             }
-            result.add(line.toString());
+
+            res.add(line.toString());
             i = j;
         }
-        return result;
+
+        return res;
     }
 }
