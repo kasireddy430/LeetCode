fcceb5d4b5276c8620fa46dfe223636f3f455fea
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-17 15:22:43 -0700

    Time: 2 ms (99.79%), Space: 49.6 MB (62.53%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,40 +1,30 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        //Calculate row and column length of 2-D array
-        int ROWS = grid.length;
-        int COLS = grid[0].length;
-
+        int m = grid.length;
+        int n = grid[0].length;
         int islandsCount = 0;
 
-        //Iterate over each cell in input array
-        for(int r = 0; r < ROWS; r++){
-            for(int c = 0; c < COLS; c++){
-                //Check if the is island. If so, increment the island count
-                //and mark adjacent islands visited(by marking it as water)
-                if(grid[r][c] == '1'){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
                     islandsCount++;
-                    visitIslands(grid, r, c);
+                    dfs(grid, i, j);
                 }
             }
         }
         return islandsCount;
     }
 
-    private void visitIslands(char[][] grid, int r, int c){
-        //Base case: check if the row or column is invalid and the cell is water
-        if(r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == '0'){
-            //If so the cell isn't required to be marked as visited
+    private void dfs(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0'){
             return;
         }
 
-        //If not, mark the cell visited
-        grid[r][c] = '0';
-
-        //Visit the adjacent cells of the current island and mark them visited
-        visitIslands(grid, r + 1, c);
-        visitIslands(grid, r - 1, c);
-        visitIslands(grid, r, c + 1);
-        visitIslands(grid, r, c - 1);
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
 
