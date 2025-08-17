fb236fdaee2c6274b529af4ad7f6344de35edf4e
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-17 10:27:39 -0700

    Time: 0 ms (100%), Space: 44.3 MB (87.07%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -16,44 +16,31 @@
 class Solution {
     //Perform Morris In-order traversal for finding the kth smallest element
     public int kthSmallest(TreeNode root, int k) {
-        //Initialize cur pointer at root node
         TreeNode cur = root;
-
-        //Iterate until cur reaches null
         while(cur != null){
-            //Check if left subtree doesn't exist
             if(cur.left == null){
-                //If so we visit the current node and move to right subTree
                 k--;
-                if(k==0)
-                    return cur.val;
-                cur  = cur.right;
-            } else {
-                //If not identify the predecessor(rightmost node in left subtree)
+                if(k == 0) return cur.val;
+                cur = cur.right;
+            } else{
                 TreeNode predecessor = cur.left;
 
-                while(predecessor.right != null &&predecessor.right != cur){
+                while(predecessor.right != null && predecessor.right != cur){
                     predecessor = predecessor.right;
                 }
 
-                //Check if predecessor doesn't have a right child
                 if(predecessor.right == null){
-                    //If so, create a temporary thread to the cur node from predecessor and move 
-                    //from current node to left child
                     predecessor.right = cur;
                     cur = cur.left;
                 } else{
-                    //If not, remove the temporary thread and visit current node and
-                    //move from current node to the right child
                     predecessor.right = null;
                     k--;
-                    if(k==0)
-                        return cur.val;
+                    if(k == 0) return cur.val;
                     cur = cur.right;
                 }
             }
         }
-        //return -1, if kth smallest element isn't found
+
         return -1;
     }
 }
