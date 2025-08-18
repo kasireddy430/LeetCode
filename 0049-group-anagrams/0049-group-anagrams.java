f140eced15c3b865b2c76f41174638c983780760
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-18 16:59:18 -0700

    Time: 19 ms (18.39%), Space: 49.2 MB (10.04%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,27 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //Create a hashmap to store the list of anagram strings for a given key
-        Map<String, List<String>> anagram = new HashMap<>();
+        Map<String, List<String>> map = new HashMap<>();
 
-        //Iterate over input array
         for(String str : strs){
-            //Initialize a count array for count the characters frequencies
             int[] count = new int[26];
-
             for(char c : str.toCharArray()){
                 count[c - 'a']++;
             }
 
-            //Convert character count array to a string and use it as a key
             String key = Arrays.toString(count);
-            //Check if a given key does exists or not. If exists add the string to list of strings.
-            //If not, compute an arrayList and then add the string for the given key
-            anagram.putIfAbsent(key, new ArrayList<>());
-            anagram.get(key).add(str);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
         }
-        //return the list of anagram's grouped 
-        return new ArrayList<>(anagram.values());
-    }
+
+        return new ArrayList<>(map.values());
+        }
 }
 
 //Time Complexity: O(m * n)
