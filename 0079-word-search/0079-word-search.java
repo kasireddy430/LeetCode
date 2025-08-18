48880461483abc11c8a255f7f2bce88e91192c87
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-17 23:24:47 -0700

    Time: 131 ms (82.72%), Space: 42 MB (29.59%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int row = board.length;
-        int col = board[0].length;
+        int m = board.length;
+        int n = board[0].length;
 
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n; j++){
                 if(dfs(board, word, i, j, 0)){
                     return true;
                 }
             }
         }
+
         return false;
     }
 
@@ -18,18 +19,21 @@ class Solution {
             return true;
         }
 
-        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length || 
-        board[r][c] != word.charAt(i) || board[r][c] == '#'){
+        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length ||
+        word.charAt(i) != board[r][c] || board[r][c] == '#'){
             return false;
         }
 
+        char tmp = board[r][c];
         board[r][c] = '#';
-        boolean res = dfs(board, word, r + 1, c, i + 1) ||
-                    dfs(board, word, r - 1, c, i + 1) ||
-                    dfs(board, word, r, c + 1, i + 1) ||
-                    dfs(board, word, r, c - 1, i + 1);
 
-        board[r][c] = word.charAt(i);
+        boolean res = dfs(board, word, r + 1, c, i + 1) ||
+        dfs(board, word, r - 1, c, i + 1) ||
+        dfs(board, word, r, c + 1, i + 1) ||
+        dfs(board, word, r, c - 1, i + 1);
+
+        board[r][c] = tmp;
+
         return res;
     }
 }
