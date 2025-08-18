e1fb88a168b0e6f5ac16a315a362f6c05aaf0352
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-17 18:45:42 -0700

    Time: 35 ms (74.78%), Space: 58 MB (60.68%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -3,8 +3,9 @@ class Solution {
     public int longestSubarray(int[] nums, int limit) {
         Deque<Integer> max = new LinkedList<>();
         Deque<Integer> min = new LinkedList<>();
-        int longest = 0;
-        int l = 0, r = 0;
+
+        int r = 0;
+        int l = 0, longest = 0;
 
         while(r < nums.length){
             while(!max.isEmpty() && max.getLast() < nums[r]){
@@ -19,7 +20,7 @@ class Solution {
 
             min.addLast(nums[r]);
 
-            while(max.getFirst() - min.getFirst() > limit){
+            while((max.getFirst() - min.getFirst()) > limit){
                 if(max.getFirst() == nums[l]){
                     max.removeFirst();
                 }
@@ -27,8 +28,10 @@ class Solution {
                 if(min.getFirst() == nums[l]){
                     min.removeFirst();
                 }
+
                 l++;
             }
+
             longest = Math.max(longest, r - l + 1);
             r++;
         }
