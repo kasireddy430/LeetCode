baac748eea16f2c4bb851d3a4fdcb8994d23ca18
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-17 18:43:41 -0700

    Time: 35 ms (74.78%), Space: 58.4 MB (50.76%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,39 +1,41 @@
 class Solution {
 
     public int longestSubarray(int[] nums, int limit) {
-        Deque<Integer> maxDeque = new LinkedList<>();
-        Deque<Integer> minDeque = new LinkedList<>();
-        int left = 0;
-        int maxLength = 0;
+        Deque<Integer> max = new LinkedList<>();
+        Deque<Integer> min = new LinkedList<>();
+        int longest = 0;
+        int l = 0, r = 0;
 
-        for (int right = 0; right < nums.length; ++right) {
-            // Maintain the maxDeque in decreasing order
-            while (!maxDeque.isEmpty() && maxDeque.peekLast() < nums[right]) {
-                maxDeque.pollLast();
+        while(r < nums.length){
+            while(!max.isEmpty() && max.getLast() < nums[r]){
+                max.removeLast();
             }
-            maxDeque.offerLast(nums[right]);
 
-            // Maintain the minDeque in increasing order
-            while (!minDeque.isEmpty() && minDeque.peekLast() > nums[right]) {
-                minDeque.pollLast();
+            max.addLast(nums[r]);
+
+            while(!min.isEmpty() && min.getLast() > nums[r]){
+                min.removeLast();
             }
-            minDeque.offerLast(nums[right]);
 
-            // Check if the current window exceeds the limit
-            while (maxDeque.peekFirst() - minDeque.peekFirst() > limit) {
-                // Remove the elements that are out of the current window
-                if (maxDeque.peekFirst() == nums[left]) {
-                    maxDeque.pollFirst();
+            min.addLast(nums[r]);
+
+            while(max.getFirst() - min.getFirst() > limit){
+                if(max.getFirst() == nums[l]){
+                    max.removeFirst();
                 }
-                if (minDeque.peekFirst() == nums[left]) {
-                    minDeque.pollFirst();
-                }
-                ++left;
-            }
 
-            maxLength = Math.max(maxLength, right - left + 1);
+                if(min.getFirst() == nums[l]){
+                    min.removeFirst();
+                }
+                l++;
+            }
+            longest = Math.max(longest, r - l + 1);
+            r++;
         }
 
-        return maxLength;
+        return longest;
     }
 }
+
+//TC : O(n)
+//SC : O(n)
