e1ae76cd574cf57282d05f127b7cd53da1dd457d
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-18 17:17:01 -0700

    Time: 1 ms (100%), Space: 44.5 MB (52.69%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,35 +10,26 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        //Create a dummy node to simplify the result list
         ListNode dummy = new ListNode(0);
         ListNode node = dummy;
+        int cary = 0;
 
-        //Initialize a variable to handle values greater than 9
-        int carry = 0;
+        while(l1 != null || l2 != null || cary != 0){
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
 
-        //Iterate over both the lists
-        while(l1 != null || l2 != null || carry != 0){
-            //calculate the current node's values from both the lists
-            int v1 = (l1 != null) ? l1.val : 0;
-            int v2 = (l2 != null) ? l2.val : 0;
+            int result = val1 + val2 + cary;
 
-
-            int result = v1 + v2 + carry;
-            //Update the value to use it next iteration
-            carry = result/10;
-            //calculate the value that needs to be added to the list
-            result = result%10;
+            cary = result / 10;
+            result = result % 10;
 
             node.next = new ListNode(result);
-            //Move pointer to next node
             node = node.next;
 
-            //Move from curent node's to next node's in both the lists 
             l1 = (l1 != null) ? l1.next : null;
             l2 = (l2 != null) ? l2.next : null;
         }
-        //return the head of new list
+
         return dummy.next;
     }
 }
