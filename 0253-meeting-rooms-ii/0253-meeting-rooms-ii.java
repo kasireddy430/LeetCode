145a9bb217cd9d763c8957c1e823c1319d4c359c
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-18 17:48:37 -0700

    Time: 8 ms (67.2%), Space: 46.3 MB (28.8%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,29 +1,20 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        //Base Case: Check if there are any meetings
-        if(intervals.length == 0){
-            return 0;
-        }
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-        //Sort intervals based on their start time
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        //For tracking the meeting end times
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        //Iterate through intervals
+        PriorityQueue<Integer> q = new PriorityQueue<>();
         for(int[] interval : intervals){
-            //Check if earliest meeting is ended before current meeting
-            if(!minHeap.isEmpty() && minHeap.peek() <= interval[0]){
-                //If so remove earliest meeting end time from minHeap
-                minHeap.poll();
+            int start = interval[0];
+            int end = interval[1];
+
+            if(!q.isEmpty() && q.peek() <= start){
+                q.poll();
             }
-            //Add current meeting's end time to heap
-            minHeap.offer(interval[1]);
+
+            q.offer(end);
         }
 
-        //minHeap's size tells us the number of concurrent meetings
-        return minHeap.size();
+        return q.size();
     }
 }
 
