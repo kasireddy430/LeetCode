013d0ee9e2ca9cfdc23e58424e7cd5d3156fabe7
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-22 12:21:46 -0700

    Time: 3 ms (61.4%), Space: 47.2 MB (70.48%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -9,7 +9,7 @@
  *
  * Time Complexity: O(M*N)
  *
- * Space Complexity: O(1) without considering result space.
+ * Space Complexity: O(M*N) considering result space.
  *
  * M = Number of rows. N = Number of columns.
  */
@@ -17,41 +17,36 @@ class Solution {
     public int[] findDiagonalOrder(int[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
-        int[] result = new int[rows * cols];
+
+        int[] result = new int[rows*cols];
         int r = 0;
         int c = 0;
 
-        for (int i = 0; i < result.length; i++) {
+        for(int i = 0; i < result.length; i++){
             result[i] = matrix[r][c];
-            if ((r + c) % 2 == 0) { // Move Up
-                if (c == cols - 1) {
-                    // Reached last column. Now move to below cell in the same column.
-                    // This condition needs to be checked first due to top right corner cell.
+            if((r + c) % 2 == 0){//goes up
+                if(c == cols-1){
                     r++;
-                } else if (r == 0) {
-                    // Reached first row. Now move to next cell in the same row.
+                } else if(r == 0){
                     c++;
-                } else {
-                    // Somewhere in middle. Keep going up diagonally.
+                } else{
                     r--;
                     c++;
                 }
-            } else { // Move Down
-                if (r == rows - 1) {
-                    // Reached last row. Now move to next cell in same row.
-                    // This condition needs to be checked first due to bottom left corner cell.
+            } else {
+                if(r == rows - 1){
                     c++;
-                } else if (c == 0) {
-                    // Reached first columns. Now move to below cell in the same column.
+                } else if (c == 0){
                     r++;
                 } else {
-                    // Somewhere in middle. Keep going down diagonally.
                     r++;
                     c--;
                 }
             }
         }
-
         return result;
     }
 }
+
+//TC: O(m*n)
+//SC: O(m*n)
