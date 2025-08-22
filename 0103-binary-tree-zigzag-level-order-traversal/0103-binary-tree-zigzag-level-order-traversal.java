88d82631d866591e67a75d14b1be2fc53e08876c
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-22 09:39:51 -0700

    Time: 0 ms (100%), Space: 42.2 MB (58.63%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -28,35 +28,58 @@
  *     }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
+        if (root == null) return result; // edge case: empty tree
 
-        Deque<TreeNode> deque = new LinkedList<>();
-        deque.add(root);
-        boolean reverse = false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root); // start BFS with root
+        boolean reverse = false; // flag to control zigzag order
 
-        while (!deque.isEmpty()) {
-            int size = deque.size();
-            List<Integer> level = new ArrayList<>(size);
+        while (!queue.isEmpty()) {
+            int size = queue.size(); // number of nodes in this level
+            LinkedList<Integer> level = new LinkedList<>(); // store values for this level
 
+            // Process all nodes at the current level
             for (int i = 0; i < size; i++) {
-                if (!reverse) {
-                    TreeNode node = deque.pollFirst();
-                    level.add(node.val);
-                    if (node.left != null) deque.addLast(node.left);
-                    if (node.right != null) deque.addLast(node.right);
+                TreeNode node = queue.poll(); // take next node
+
+                // Add value depending on traversal direction
+                if (reverse) {
+                    level.addFirst(node.val); // reverse: add to front
                 } else {
-                    TreeNode node = deque.pollLast();
-                    level.add(node.val);
-                    if (node.right != null) deque.addFirst(node.right);
-                    if (node.left != null) deque.addFirst(node.left);
+                    level.addLast(node.val); // normal: add to end
                 }
+
+                // Push children into the queue for next level
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
-            reverse = !reverse;
+
+            // Save this level into result
             result.add(level);
+
+            // Flip direction for the next level
+            reverse = !reverse;
         }
+
         return result;
     }
 }
