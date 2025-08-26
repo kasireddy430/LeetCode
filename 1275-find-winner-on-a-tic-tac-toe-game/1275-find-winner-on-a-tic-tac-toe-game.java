4049a0cdc0b30f0014ee1af598d4e0243efde817
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-25 17:11:41 -0700

    Time: 0 ms (100%), Space: 41.4 MB (52.46%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.java
@@ -2,27 +2,34 @@ class Solution {
     public String tictactoe(int[][] moves) {
         char[][] board = new char[3][3];
 
-        // Fill the board
-        boolean isA = true;
-        for (int[] move : moves) {
-            board[move[0]][move[1]] = isA ? 'X' : 'O';
-            isA = !isA;
+        boolean isX = true;
+        for(int[] move : moves){
+            board[move[0]][move[1]] = isX ? 'X' : 'O';
+            isX = !isX;
         }
 
-        // Check rows and columns
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2])
-                return board[i][0] == 'X' ? "A" : "B";
-            if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i])
-                return board[0][i] == 'X' ? "A" : "B";
+        //Check row and columns
+        for(int i = 0; i < 3; i++){
+            
+            if(board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+               return board[i][0] == 'X' ? "A" : "B";
+            }
+            
+            if(board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+               return board[0][i] == 'X' ? "A" : "B";
+            }
         }
 
-        // Check diagonals
-        if (board[1][1] != 0 &&
-            ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
-             (board[0][2] == board[1][1] && board[1][1] == board[2][0])))
+        //Check diagonal's
+
+        if((board[1][1] != 0) && ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+        (board[0][2] == board[1][1] && board[2][0] == board[1][1]))){
             return board[1][1] == 'X' ? "A" : "B";
+        }
 
         return moves.length == 9 ? "Draw" : "Pending";
     }
 }
+
+//TC: O(1)
+//SC: O(1)
