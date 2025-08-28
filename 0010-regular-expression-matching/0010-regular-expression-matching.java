4fa83fd518aecf71c34c2873e1414c71a9128d7d
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-28 13:51:16 -0700

    Time: 1 ms (100%), Space: 42.2 MB (63.52%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,28 +1,34 @@
 class Solution {
-    public boolean solveMem(int i, int j, String inp, String p, Boolean[][] dp){
-        //if j become out of index, then inp should also become, if both are true then return true else false
-        if(j == p.length()){
-            return i == inp.length();
-        }
-        if(dp[i][j] != null) return dp[i][j];
-        //we will check if 1st char of both the strings are matching or not, 
-        //they will also match if p's 0th char is '.',
-        //but before checking, we will insure that our input's length should not be 0
-        boolean firstCharMatch = false;
-        if((i < inp.length())  && (inp.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) firstCharMatch = true;
-        //Now we will check, if 1st index char of p is equal to star, then we have 2 options, take that star or not take
-        if(j+1 < p.length() && p.charAt(j+1)=='*'){
-            //if we are taking *, we will check the 0th index char is match or not, then we will call solve for 1st index input
-            boolean take = firstCharMatch == true && solveMem(i+1, j, inp, p, dp); 
-            //if we are not taking *, then we will try next char after star in p, inp will remain same
-            boolean notTake = solveMem(i, j+2, inp, p, dp); 
-            return dp[i][j] = take || notTake;
-        }
-        //else it means p's 1st index char is not star, so we will do normal checking, after matching 0th index char
-            return dp[i][j] = firstCharMatch == true && solveMem(i+1, j+1, inp, p, dp);
-    }
     public boolean isMatch(String s, String p) {
-        Boolean[][] dp = new Boolean[s.length() + 1][p.length() + 1]; 
-        return solveMem(0, 0, s, p, dp);
+        Boolean[][] memo = new Boolean[s.length() + 1][p.length() + 1];
+        return solve(0, 0, s, p, memo);
+    }
+
+    private boolean solve(int i, int j, String s, String p, Boolean[][] memo){
+        if(j == p.length()){
+            return i == s.length();
+        }
+
+        if(memo[i][j] != null){
+            return memo[i][j];
+        }
+
+        boolean firstCharMatch = false;
+
+        if(i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')){
+            firstCharMatch = true;
+        }
+
+        if(j + 1 < p.length() && p.charAt(j + 1) == '*'){
+            boolean take = firstCharMatch && solve(i + 1, j, s, p, memo);
+            boolean skip = solve(i, j + 2, s, p, memo);
+            return memo[i][j] = take || skip;
+        }
+
+        return memo[i][j] = firstCharMatch && solve(i + 1, j + 1, s, p, memo);
     }
 }
+
+
+//Time Complexity: O(m * n)
+//Space Complexity: O(m * n)
