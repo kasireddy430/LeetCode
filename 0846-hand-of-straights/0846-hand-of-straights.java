23254db6e1c052da9b0c07f3ac46fc4b48ea174f
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-30 17:31:13 -0500

    Time: 22 ms (92.41%), Space: 45.8 MB (36.59%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,35 +1,44 @@
-class Solution {
-    public boolean findsucessors(int[] hand, int groupSize, int i, int n) {
-        int f = hand[i] + 1;
-        hand[i] = -1;
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == f) {
-                f = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        if (count != groupSize)
-            return false;
-        else
-            return true;
-    }
-
+public class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        int n = hand.length;
-        if (n % groupSize != 0)
-            return false;
-        Arrays.sort(hand);
-        int i = 0;
-        for (; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findsucessors(hand, groupSize, i, n))
-                    return false;
+        // If total number of cards is not divisible by groupSize, it's impossible to divide them evenly
+        if (hand.length % groupSize != 0) return false;
+
+        // Map to count frequency of each card
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : hand) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        // Try to form groups from each card in the hand
+        for (int num : hand) {
+            int start = num;
+
+            // Move backward to find the lowest consecutive number that still has a count
+            while (count.getOrDefault(start - 1, 0) > 0) start--;
+
+            // Try forming groups starting from this 'start' value up to 'num'
+            while (start <= num) {
+
+                // While the current start card still has unused copies
+                while (count.getOrDefault(start, 0) > 0) {
+                    // Attempt to build a full group of size groupSize starting from 'start'
+                    for (int i = start; i < start + groupSize; i++) {
+                        // If any card in the sequence is missing, return false
+                        if (count.getOrDefault(i, 0) == 0) return false;
+                        // Otherwise, reduce its count
+                        count.put(i, count.get(i) - 1);
+                    }
+                }
+
+                // Move to next potential group start
+                start++;
             }
         }
+
+        // All cards have been used in valid groups
         return true;
     }
 }
+
+//Time Complexity: O(n log n). Contributed by insertions into treeMap
+//Space Complexity: O(n)
