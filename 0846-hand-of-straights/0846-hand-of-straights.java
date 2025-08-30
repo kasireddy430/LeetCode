ff9e5266b417e4c26b5a233f6cd7c2b5599460b7
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-08-30 17:31:29 -0500

    Time: 64 ms (20.59%), Space: 46.2 MB (14.93%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.java
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,44 +1,22 @@
-public class Solution {
-    public boolean isNStraightHand(int[] hand, int groupSize) {
-        // If total number of cards is not divisible by groupSize, it's impossible to divide them evenly
-        if (hand.length % groupSize != 0) return false;
-
-        // Map to count frequency of each card
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : hand) {
-            count.put(num, count.getOrDefault(num, 0) + 1);
+class Solution {
+    public boolean isNStraightHand(int[] nums, int k) {
+        if(nums.length % k != 0){
+            return false;
         }
-
-        // Try to form groups from each card in the hand
-        for (int num : hand) {
-            int start = num;
-
-            // Move backward to find the lowest consecutive number that still has a count
-            while (count.getOrDefault(start - 1, 0) > 0) start--;
-
-            // Try forming groups starting from this 'start' value up to 'num'
-            while (start <= num) {
-
-                // While the current start card still has unused copies
-                while (count.getOrDefault(start, 0) > 0) {
-                    // Attempt to build a full group of size groupSize starting from 'start'
-                    for (int i = start; i < start + groupSize; i++) {
-                        // If any card in the sequence is missing, return false
-                        if (count.getOrDefault(i, 0) == 0) return false;
-                        // Otherwise, reduce its count
-                        count.put(i, count.get(i) - 1);
+        Map<Integer, Integer> freq = new TreeMap<>();
+        for(int i : nums){
+            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        }
+        for(int i : freq.keySet()){
+            while(freq.get(i) > 0){
+                for(int j=0; j<k; j++){
+                    if(freq.getOrDefault(j + i, 0) <= 0){
+                        return false;
                     }
+                    freq.put(i + j, freq.get(i + j) - 1);
                 }
-
-                // Move to next potential group start
-                start++;
             }
         }
-
-        // All cards have been used in valid groups
         return true;
     }
 }
-
-//Time Complexity: O(n log n). Contributed by insertions into treeMap
-//Space Complexity: O(n)
