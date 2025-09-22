9e1c000661c3ab631c4a15ba6eb66ecb54268281
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-09-21 19:24:34 -0500

    Time: 6 ms (82.87%), Space: 44.6 MB (35.54%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,30 +1,35 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
-        for (int[] flight : flights) {
-            adj.get(flight[0]).add(new int[] {flight[1], flight[2]});
+
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
         }
 
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {src, 0, 0}); // {node, cost, stops}
+        for(int[] flight : flights){
+            adj.get(flight[0]).add(new int[]{flight[1], flight[2]});
+        }
 
         int[] minCost = new int[n];
         Arrays.fill(minCost, Integer.MAX_VALUE);
-        minCost[src] = 0;
 
-        while (!q.isEmpty()) {
-            int[] curr = q.poll();
-            int node = curr[0], cost = curr[1], stops = curr[2];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{src, 0, 0}); //(currentNode, totalCost, stops)
 
-            if (stops > k) continue;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int node = cur[0], cost = cur[1], stops = cur[2];
 
-            for (int[] neighbor : adj.get(node)) {
-                int nextNode = neighbor[0], price = neighbor[1];
+            if(stops > k){
+                continue;
+            }
 
-                if (cost + price < minCost[nextNode]) {
-                    minCost[nextNode] = cost + price;
-                    q.offer(new int[] {nextNode, minCost[nextNode], stops + 1});
+            for(int[] nei: adj.get(node)){
+                int neighbor = nei[0], price = nei[1];
+
+                if(stops <= k && minCost[neighbor] > cost + price){
+                    minCost[neighbor] = cost + price;
+                    q.offer(new int[]{neighbor, minCost[neighbor], stops + 1});
                 }
             }
         }
@@ -32,3 +37,6 @@ class Solution {
         return minCost[dst] == Integer.MAX_VALUE ? -1 : minCost[dst];
     }
 }
+
+//Time Complexity: O(n + E * k)
+//Space Complexity: O(n + E)
