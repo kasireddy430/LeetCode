917a7bc5590bbe205c354de125462e65d6aaf654
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-09-25 11:07:45 -0500

    Time: 23 ms (72.54%), Space: 46.1 MB (17.92%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.java
@@ -1,9 +1,22 @@
 class Solution {
     public String arrangeWords(String text) {
-        String[] words = text.split(" ");
-        words[0] = words[0].toLowerCase();
-        Arrays.sort(words, Comparator.comparingInt(String::length));
-        words[0] = words[0].substring(0, 1).toUpperCase() + words[0].substring(1);
-        return String.join(" ", words);
+        //Array of string's seperated by spaces
+        String[] strs = text.split(" ");
+
+        //Update first string to lower case
+        strs[0] = strs[0].toLowerCase();
+
+        //sort them based on their lengths
+        Arrays.sort(strs, (a, b) -> a.length() - b.length());
+
+        //update first charcter of first string to be upper case
+        strs[0] = strs[0].substring(0, 1).toUpperCase() + strs[0].substring(1);
+
+        //return sorted array as string
+        return String.join(" ", strs).trim();
+
     }
 }
+
+//TC:
+//SC: 
