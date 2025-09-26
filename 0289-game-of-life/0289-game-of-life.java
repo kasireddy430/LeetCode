282fdbface7305529583a016b22c792a9f486ff9
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-09-26 16:19:34 -0500

    Time: 0 ms (100%), Space: 42.1 MB (15.05%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,52 +1,48 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        int m = board.length;
+        int n = board[0].length;
 
-        // First pass: **Mark** temporary state changes
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int liveNeighbors = countLiveNeighbors(board, i, j);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int neighborCount = findNeighbors(board, i , j);
 
-                if (board[i][j] == 1) { // Current cell is live
-                    if (liveNeighbors < 2 || liveNeighbors > 3) {
-                        board[i][j] = 2; // live -> dead
+                if(board[i][j] == 1){
+                    if(neighborCount < 2 || neighborCount > 3){
+                        board[i][j] = 2; //live -> dead
                     }
-                } else { // Current cell is dead
-                    if (liveNeighbors == 3) {
-                        board[i][j] = 3; // dead -> live
+                } else {
+                    if(neighborCount == 3){
+                        board[i][j] = 3; //dead -> live
                     }
                 }
             }
         }
 
-        // Second pass: **Finalize** state changes
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 2) {
-                    board[i][j] = 0; // Finalize live -> dead
-                } else if (board[i][j] == 3) {
-                    board[i][j] = 1; // Finalize dead -> live
+            for(int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(board[i][j] == 2){
+                        board[i][j] = 0;
+                    } else if(board[i][j] == 3){
+                        board[i][j] = 1;
+                    }
                 }
             }
-        }
+        
     }
 
-    private int countLiveNeighbors(int[][] board, int i, int j) {
+    private int findNeighbors(int[][] board, int i, int j){
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {-1, -1}, {1, -1}, 
+                            {-1, 1}, {1, 1}};
+
         int count = 0;
-        int n = board.length;
-        int m = board[0].length;
 
-        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
-        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        for(int[] dir : directions){
+            int r = i + dir[0];
+            int c = j + dir[1];
 
-        for (int d = 0; d < 8; d++) {
-            int ni = i + dx[d];
-            int nj = j + dy[d];
-
-            if (ni >= 0 && ni < n && nj >= 0 && nj < m) {
-                // Neighbors that were originally live
-                if (board[ni][nj] == 1 || board[ni][nj] == 2) {
+            if(r >= 0 && r < board.length && c >= 0 && c < board[0].length){
+                if(board[r][c] == 1 || board[r][c] == 2){
                     count++;
                 }
             }
@@ -54,3 +50,6 @@ class Solution {
         return count;
     }
 }
+
+//TC: O(mn)
+//SC: O(1)
