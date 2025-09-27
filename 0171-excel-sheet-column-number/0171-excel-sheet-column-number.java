73c35a5e7e51f9b56357817d4e5bd32899da8258
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-09-27 12:04:04 -0500

    Time: 1 ms (89.46%), Space: 42.7 MB (5.44%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+
+        for(char c : columnTitle.toCharArray()){
+            result = result * 26 + (c - 'A' + 1);
+        }
+
+        return result;
+    }
+}
+
+//TC: O(n)
+//SC: O(1)
