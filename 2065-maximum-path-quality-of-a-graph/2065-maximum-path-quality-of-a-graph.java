c0fdaf0cc1aa0a238df82ca63be11679b08a5fac
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-10-22 17:53:08 -0500

    Time: 107 ms (82.82%), Space: 45.4 MB (71.78%) - LeetHub

--- 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.java
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.java
@@ -1,38 +1,41 @@
-class Solution {
-	public int maximalPathQuality(int[] values, int[][] edges, int maxTime) {
-        List<List<int[]>> adjList = new ArrayList();
-        for(int i : values) {
-            adjList.add(new ArrayList());
+public class Solution {
+    private int ans;
+
+    public int maximalPathQuality(int[] values, int[][] edges, int maxTime) {
+        int n = values.length;
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
+        // Build graph
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[]{e[1], e[2]});
+            graph[e[1]].add(new int[]{e[0], e[2]});
         }
-        
-        for(int edge[] : edges) {
-            adjList.get(edge[0]).add(new int[] {edge[1], edge[2]});
-            adjList.get(edge[1]).add(new int[] {edge[0], edge[2]});
-        }
-        int[] visited = new int[values.length];
-        solve(values, adjList, visited, 0, maxTime, 0, 0);
+
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+
+        ans = 0;
+        dfs(0, visited, values[0], maxTime, values, graph);
         return ans;
     }
-    int ans;
-    
-    void solve(int[] values, List<List<int[]>> adjList, int[] visited, int node, int maxTime, int currTime, int score) {
-        if(currTime > maxTime) {
-            return;
+
+    private void dfs(int node, boolean[] visited, int gain, int timeLeft, int[] values, List<int[]>[] graph) {
+        if (node == 0) {
+            ans = Math.max(ans, gain);
         }
-        if(visited[node] == 0) {
-            score += values[node];
+
+        for (int[] edge : graph[node]) {
+            int nei = edge[0], w = edge[1];
+            if (w <= timeLeft) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    dfs(nei, visited, gain + values[nei], timeLeft - w, values, graph);
+                    visited[nei] = false; // backtrack
+                } else {
+                    dfs(nei, visited, gain, timeLeft - w, values, graph);
+                }
+            }
         }
-        
-        if(node == 0) {
-            ans = Math.max(ans, score);
-        }
-        
-        visited[node]++;
-        
-        for(int[] v : adjList.get(node)) {
-            solve(values, adjList, visited, v[0], maxTime, currTime + v[1], score);
-        }
-        
-        visited[node]--;
     }
 }
