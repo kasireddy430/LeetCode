3275768c28f25c0406cf5e793db732b3ca499f13
Venkata Akshith Reddy Kasireddy <kvakshithreddy@gmail.com>
2025-10-22 20:17:19 -0500

    Time: 1 ms (99.4%), Space: 57.3 MB (32.45%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,25 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int maxSum = nums[0];
-        int curSum = nums[0];
+        //Initialize maxSum with first element of input array(such that subarray 
+        //maxSum will consider atleast one element from input array). Initilize curSum
+        //to track currrent sum in the subarray
+        int maxSum = nums[0], curSum = 0;
 
-        for (int i = 1; i < nums.length; i++) {
-            curSum = Math.max(nums[i], curSum + nums[i]);
+        //Iterate over input array
+        for(int num : nums){
+            //Check if current sum is negative. if so reset curSum to 0.
+            if(curSum < 0){
+                curSum = 0;
+            }
+            //Add current num to current Sum
+            curSum += num;
+            //Update maxSum if curSum is greater
             maxSum = Math.max(maxSum, curSum);
         }
-
+        //Return maximum subarray sum
         return maxSum;
     }
 }
-
 
 //Time Complexity: O(n)
 //Space Complexity: O(1)
